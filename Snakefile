fe852fb37e445a09322c26bf1ac4f9f792fe31a0
bradford roarr <bradford_roarr@brown.edu>
2022-04-15 16:10:47 -0400

    feat: snakemake data pipeline framework
    This commit introduces a snakemake framework and a CLI script that exposes the functionality of the IceFloeTracker.jl module. This work assumes that we will be processing batches of files on the same node. We may find later that we want to rework out pipeline so that single images are processed on nodes, and those images are broken down into many tiles that are then worked on in parallel.

---
+++ Snakefile
@@ -0,0 +1,25 @@
+rule all:
+  input: "data/output/landmask", "data/output/cloudmask"
+
+rule fetchdata:
+  output: 
+    directory("data/input"),
+    directory("data/input/landmask"), 
+    directory("data/input/truecolor"),
+    directory("data/input/reflectance"),
+    "data/input/metadata.json"
+  shell: "./scripts/ice-floe-tracker.jl fetchdata {output[0]}"
+
+rule landmask:
+  input: 
+    metadata="data/input/metadata.json",
+    images="data/input/landmask"
+  output: directory("data/output/landmask")
+  shell: "./scripts/ice-floe-tracker.jl landmask {input.metadata} {input.images} {output}"
+
+rule cloudmask:
+  input: 
+    metadata="data/input/metadata.json",
+    images="data/input/reflectance"
+  output: directory("data/output/cloudmask")
+  shell: "./scripts/ice-floe-tracker.jl cloudmask {input.metadata} {input.images} {output}"
--- src/IceFloeTracker.jl
+++ src/IceFloeTracker.jl
@@ -1,5 +1,35 @@
 module IceFloeTracker
 
-# Write your package code here.
+function fetchdata(;output::AbstractString)
+  mkpath("$output")
+  touch("$output/metadata.json")
+
+  mkpath("$output/landmask")
+  touch("$output/landmask/landmask.tiff")
+
+  mkpath("$output/truecolor")
+  touch("$output/truecolor/a.tiff")
+  touch("$output/truecolor/b.tiff")
+  touch("$output/truecolor/c.tiff")
+
+  mkpath("$output/reflectance")
+  touch("$output/reflectance/a.tiff")
+  touch("$output/reflectance/b.tiff")
+  touch("$output/reflectance/c.tiff")
+end
+
+function landmask(;metadata::AbstractString, input::AbstractString, output::AbstractString)
+  mkpath("$output")
+  touch("$output/a.tiff")
+  touch("$output/b.tiff")
+  touch("$output/c.tiff")
+end
+
+function cloudmask(;metadata::AbstractString, input::AbstractString, output::AbstractString)
+  mkpath("$output")
+  touch("$output/a.tiff")
+  touch("$output/b.tiff")
+  touch("$output/c.tiff")
+end
 
 end
