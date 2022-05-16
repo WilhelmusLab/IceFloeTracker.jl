6024a36a1eeb3a126911e11d52c32fb5c191c756
Timothy Divoll <timothy.divoll@gmail.com>
2022-05-16 16:18:18 -0400

    feat: update function with disk strel, use @test_approx_eq_sigma_eps to test created landmask

--- src/landmask.jl
+++ src/landmask.jl
@@ -1,19 +1,20 @@
 """
     create_landmask(landmask_image, num_pixels_dilate, num_pixels_closing)
 
-Convert a 3-channel RGB land mask TIFF to a 1-channel binary TIFF, including a buffer to extend the land over any soft ice regions; land = 0, water/ice = 1.
+Convert a 3-channel RGB land mask image to a 1-channel binary matrix, including a buffer to extend the land over any soft ice regions; land = 0, water/ice = 1.
 
 # Arguments
-- `landmask_image`: loaded land mask TIFF from input
-- `num_pixels_dilate`: number of pixels used to extend coast line; default = 50
-- `num_pixels_closing`: number of pixels used to fill holes in land mask; default = 15
+- `landmask_image`: loaded land mask image from input
+- `struct_elem`: disk-shaped structuring element with radius of 50 pixels
+- `num_pixels_closing`: number of pixels used to fill holes in land mask; default = 50
 
 """
-function create_landmask(landmask_image::TiffImages.AbstractTIFF; num_pixels_dilate::Int=50, num_pixels_closing::Int=15)
+function create_landmask(landmask_image::Matrix{RGB{N0f8}}, struct_elem; num_pixels_closing::Int=50)
     # Drop third dimension if it exists (test image had 3 dims: height x width x 1)
-    landmask_image = dropdims(landmask_image, dims = 3)
+    # landmask_image = dropdims(landmask_image, dims = 3)
     landmask_binary = Gray.(landmask_image) .== 0
-    landmask_binary = LocalFilters.dilate(.!landmask_binary, num_pixels_dilate)
+    landmask_binary = LocalFilters.dilate(.!landmask_binary, 50)
+    # landmask_binary = ImageProjectiveGeometry.imdilate(.!landmask_binary, struct_elem)
     landmask_binary = LocalFilters.closing(landmask_binary, num_pixels_closing)
     return landmask_binary
     # update to process inline
@@ -26,11 +27,11 @@ Zero out pixels in land and soft ice regions on truecolor image, return RGB imag
 
 
 # Arguments
-- `input_image`: truecolor RGB TIFF file stored in `input/truecolor`
-- `landmask_binary`: binary landmask TIFF from `create_landmask`  
+- `input_image`: truecolor RGB image file stored in `input/truecolor`
+- `landmask_binary`: binary landmask image from `create_landmask`  
 
 """
-function apply_landmask(input_image::TiffImages.AbstractTIFF, landmask_binary::BitArray)
+function apply_landmask(input_image::Matrix{RGB{N0f8}}, landmask_binary::BitArray)
     image_masked = .!landmask_binary .* input_image
     return image_masked
     # update to process inline
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,29 +1,12 @@
 using IceFloeTracker
-using ImageIO
 using Images
 using Test
-using TiffImages
-
-lm_image = load("/Users/tdivoll/IceFloeTracker.jl/data/input/landmask.tiff")
-
-function create_landmask(landmask_image; num_pixels_dilate::Int=50, num_pixels_closing::Int=15)
-    # Drop third dimension if it exists (test image had 3 dims: height x width x 1)
-    landmask_image = dropdims(landmask_image, dims = 3)
-    landmask_binary = Gray.(landmask_image) .== 0
-    landmask_binary = LocalFilters.dilate(.!landmask_binary, num_pixels_dilate)
-    landmask_binary = LocalFilters.closing(landmask_binary, num_pixels_closing)
-    return landmask_binary
-    # update to process inline
-end
-
-masked_image = create_landmask(lm_image, 50, 15)
-
+using DelimitedFiles
 
 @testset "IceFloeTracker.jl" begin
-    @test created_landmask == matlab_landmask
-    ## Next test will return a percent difference
-    result = (@test_approx_eq_sigma_eps(created_landmask, matlab_landmask, 0, 0))
-    #expected landmask #calc 95% similar
-    result <= 0.95   
-    
+    lm_image = load("./data/landmask.tiff")
+    matlab_landmask = load("./data/matlab_landmask.png")
+    struct_elem = readdlm("../data/input/se.csv", ',', Bool)
+    masked_image = IceFloeTracker.create_landmask(lm_image, struct_elem; num_pixels_closing=50)
+    @test (@test_approx_eq_sigma_eps masked_image matlab_landmask [0,0] 0.001) === nothing 
 end
