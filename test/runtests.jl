258b9b01a437de450a9cc2784b3cbc276f2e3eb7
Timothy Divoll <timothy.divoll@gmail.com>
2022-05-20 16:38:34 -0400

    docs: fixed printouts

--- test/runtests.jl
+++ test/runtests.jl
@@ -22,6 +22,8 @@ using DelimitedFiles
         struct_elem = readdlm(strel_file, ',', Bool)
         strel_h, strel_w = ceil.(Int, size(struct_elem)./2)
         @time masked_image = IceFloeTracker.create_landmask(lm_image, struct_elem; num_pixels_closing=num_pixels_closing)
+        println("----------Run second time for JIT warmup---------")
+        @time masked_image = IceFloeTracker.create_landmask(lm_image, struct_elem; num_pixels_closing=num_pixels_closing)
 
         # test for percent difference in landmask images, ignore edges because we are not padding in Julia before applying strel_file
         @test (@test_approx_eq_sigma_eps masked_image[strel_h:end-strel_h, strel_w:end-strel_w] matlab_landmask[strel_h:end-50, strel_w:end-50] [0,0] 0.005) == nothing 
