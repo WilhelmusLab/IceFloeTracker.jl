fc832d62ba2285bb5b5cf157cf321d9a37104b79
Timothy Divoll <timothy.divoll@gmail.com>
2022-05-26 15:12:06 -0400

    test: update gitignore to allow selected test files

--- src/cloudmask.jl
+++ src/cloudmask.jl
@@ -4,23 +4,23 @@
 Convert a 3-channel false color reflectance image to a 1-channel binary matrix; clouds = 0, else = 1. Default thresholds are defined in the published Ice Floe Tracker article: Remote Sensing of the Environment 234 (2019) 111406.
 
 # Arguments
-- `reflectance_image`: corrected reflectance false color image - bands [7,2,1]
+- `ref_image`: corrected reflectance false color image - bands [7,2,1]
 
 """
-function create_cloudmask(reflectance_image::Matrix{RGB{N0f8}}; prelim_threshold::Float64=0.431, red_threshold::Float64=0.784, green_threshold::Float64=0.745, ratio_lower::Float64=0.0, ratio_upper::Float64=0.75)
+function create_cloudmask(ref_image::Matrix{RGB{N0f8}}; prelim_threshold::Float64=0.431, red_threshold::Float64=0.784, green_threshold::Float64=0.745, ratio_lower::Float64=0.0, ratio_upper::Float64=0.75)
   println("Setting thresholds")
-  reflect_working_view = channelview(reflectance_image)
-  reflect_orig_view = channelview(reflectance_image)
-  orig_view_clouds = reflect_orig_view[1,:,:] .> prelim_threshold # intensity value 110
-  mask_r = reflect_working_view[1,:,:] .< red_threshold # intensity value 200
-  mask_g = reflect_working_view[2,:,:] .> green_threshold # intensity value 190
+  ref_working_view = channelview(ref_image)
+  ref_orig_view = channelview(ref_image)
+  orig_view_clouds = ref_orig_view[1,:,:] .> prelim_threshold # intensity value 110
+  mask_r = ref_working_view[1,:,:] .< red_threshold # intensity value 200
+  mask_g = ref_working_view[2,:,:] .> green_threshold # intensity value 190
   # First find all the pixels that meet threshold logic in red and green channels
   println("Masking clouds and discriminatinhg cloud-ice")
   # Next find pixels that meet both thresholds and mask them from red and green channels
   mask_rg = mask_r .&& mask_g
-  reflect_working_view[1,:,:] = mask_rg .* reflect_working_view[1,:,:]
-  reflect_working_view[2,:,:] = mask_rg .* reflect_working_view[2,:,:]
-  cloud_ice = Float64.(reflect_working_view[1,:,:])./Float64.(reflect_working_view[2,:,:])
+  ref_working_view[1,:,:] = mask_rg .* ref_working_view[1,:,:]
+  ref_working_view[2,:,:] = mask_rg .* ref_working_view[2,:,:]
+  cloud_ice = Float64.(ref_working_view[1,:,:])./Float64.(ref_working_view[2,:,:])
   mask_cloud_ice = cloud_ice .>= ratio_lower .&& cloud_ice .< ratio_upper
   println("Creating final cloudmask")
   cloudmask = .!mask_cloud_ice .* orig_view_clouds
@@ -37,8 +37,8 @@ Zero out pixels containing clouds where clouds and ice are not discernable.
 - `cloudmask`: binary cloudmask with clouds = 0, else = 1
 
 """
-function apply_cloudmask(reflectance_image::Matrix{RGB{N0f8}}, cloudmask::BitMatrix)::Matrix{RGB{N0f8}}
-    masked_image = .!cloudmask .* reflectance_image
+function apply_cloudmask(ref_image::Matrix{RGB{N0f8}}, cloudmask::BitMatrix)::Matrix{RGB{N0f8}}
+    masked_image = .!cloudmask .* ref_image
     image_view = channelview(masked_image)
     cloudmasked_view = StackedView(zeroarray, image_view[2,:,:], image_view[3,:,:])
     cloudmasked_image = colorview(RGB, cloudmasked_view)
