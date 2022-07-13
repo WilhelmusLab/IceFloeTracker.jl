c4916f1cfb13092f097f771645d0858500c4ba07
Carlos Paniagua <cpaniaguam@gmail.com>
2022-07-13 16:10:15 -0400

    feat: image padding with fill and replicate

--- test/runtests.jl
+++ test/runtests.jl
@@ -131,14 +131,54 @@ using Dates
     @testset "utils.jl pad utilities" begin
         println("-------------------------------------------------")
         println("--------------- Pad Image Tests -----------------")
-        img_path = "/landmask.tiff"
-        img = Images.load.(test_data_dir * img_path)
-        val = 0
-        rad = 10
-        h,w = size(img)
-        paddedimg = IceFloeTracker.add_padding(img, val, rad)
-        @test size(paddedimg) == (h+2*rad,w+2*rad)
-        @test IceFloeTracker.remove_padding(paddedimg, rad) == img
+        rad = 3
+        val = 1
+        simpleimg = collect(reshape(1:4,2,2))
+        h,w = size(simpleimg)
+        # 2×2 Matrix{Int64}:
+        #  1  3
+        #  2  4
+        paddedimg = IceFloeTracker.add_padding(simpleimg, rad) # replicate with rad=3
+        # 8×8 Matrix{Int64}:
+        #  1  1  1  1  3  3  3  3
+        #  1  1  1  1  3  3  3  3
+        #  1  1  1  1  3  3  3  3
+        #  1  1  1  1  3  3  3  3
+        #  2  2  2  2  4  4  4  4
+        #  2  2  2  2  4  4  4  4
+        #  2  2  2  2  4  4  4  4
+        #  2  2  2  2  4  4  4  4
+        
+        # test the corners
+        @test paddedimg[1,1]     == 1 &&
+              paddedimg[1,end]   == 3 &&
+              paddedimg[end,1]   == 2 &&
+              paddedimg[end,end] == 4 
+        
+        paddedimg = IceFloeTracker.add_padding(simpleimg, rad, :replicate) # again, replicate with rad = 3
+        # test the corners
+        @test paddedimg[1,1]     == 1 &&
+              paddedimg[1,end]   == 3 &&
+              paddedimg[end,1]   == 2 &&
+              paddedimg[end,end] == 4 
 
-    end
+        paddedimg = IceFloeTracker.add_padding(simpleimg, rad, :fill) # pad with zeros (by default) with rad=3
+        # test the corners
+        @test paddedimg[1,1]     == 
+              paddedimg[1,end]   == 
+              paddedimg[end,1]   == 
+              paddedimg[end,end] == 0
+
+        paddedimg = IceFloeTracker.add_padding(simpleimg, 3, :fill, 1) # pad with ones with rad=3
+        @test paddedimg[1,1]     == 
+              paddedimg[1,end]   == 
+              paddedimg[end,1]   == 
+              paddedimg[end,end] == 1
+        
+        # test sizing
+        @test size(paddedimg) == (h+2*rad,w+2*rad)
+        
+        # test padding removal
+        @test IceFloeTracker.remove_padding(paddedimg, rad) == simpleimg
+    end    
 end
