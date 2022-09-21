dc6e1e69b1453b11a3dd991166af0959c956ac0c
Carlos Paniagua <cpaniaguam@gmail.com>
2022-09-21 11:35:35 -0400

    feat: psi-s after reviews
    - Add docstring to alternate method for `grad`
    - Remove `s` argument; now return `(psi, s)` always
    - Remove assert statments, change `rangeout` arg to a bool
    - Add couple extra tests for args with non-default values
    - Fix typo "cardiod" => "cardioid"

--- src/psi-s.jl
+++ src/psi-s.jl
@@ -1,15 +1,20 @@
 
 """
-    grad(x::Vector{<:Number}, y::Vector{<:Number})
+    dx, dy = grad(x::Vector{<:Number}, y::Vector{<:Number})
 
-Make gradient vector field for the set of points with coordinates in vectors `x` and `y`. Return a two column matrix with `dx` and `dy` down columns 1 and 2, respectively. 
+Make gradient vector field for the set of points with coordinates in vectors `x` and `y`. Return a tuple with `dx` and `dy` in that order. 
 """
 function grad(x::Vector{<:Number}, y::Vector{<:Number})
     dx = x[2:end] - x[1:end-1]
     dy = y[2:end] - y[1:end-1]
-    return [dx dy]
+    return dx, dy
 end
 
+"""
+    dx, dy = grad(A::Matrix{<:Number})
+
+Make gradient vector field for the set of points with coordinates in the rows of the matrix `A` with x-coordinates down column 1 and y-coordinates down column 2. Return a tuple with `dx` and `dy` in that order. 
+"""
 function grad(A::Matrix{<:Number})
     # Grab each col of A
     x,y = A[:,1], A[:,2]
@@ -17,12 +22,13 @@ function grad(A::Matrix{<:Number})
 end
 
 """
-    arclength(V)
+    arclength(x::Vector{<:Number}, y::Vector{<:Number})
 
-Return the traversed arclength along the curve represented by the points in the rows of matrix `V`.
+Return the traversed arclength along the curve represented by the points with coordinates `x` and `y`.
 """
-function arclength(V::Matrix{<:Number})
-    return cumsum([norm(collect(v_i)) for v_i in eachrow(grad(V))])
+function arclength(x::Vector{<:Number}, y::Vector{<:Number})::Vector{<:Number}
+    dx,dy = grad(x,y)
+    return cumsum([norm(collect(v_i)) for v_i in eachrow([dx dy])])
 end
 
 """
@@ -39,21 +45,16 @@ end
 
 Wrapper of `Base.atan` that returns the angle of vector (x,y) in the range [0, 2π).
 """
-function atan2(y::Vector{Float64},x::Vector{Float64})
-    ang = atan.(y,x)
-    for i=1:length(y)
-        if y[i]<0
-            ang[i] += 2*pi
-        end
+function atan2(y::Number,x::Number)
+    ang = atan(y,x)
+    if y<0
+        ang += 2*pi
     end
     return ang
 end
 
 """
-    make_psi_s(xs::Vector{Float64},
-                    ys::Vector{Float64};rangeout::Int64=0,
-                    unwrap::Bool=true,
-                    s::Bool=false)
+    make_psi_s(xs::Vector{Float64}, ys::Vector{Float64}; rangeout::Int64=0,unwrap::Bool=true, s::Bool=false)
 
 Builds the ψ-s curve defined by vectors `xs` and `ys`.
 
@@ -65,15 +66,15 @@ See also [`bwtraceboundary`](@ref), [`resample_boundary`](@ref)
 - `xs`: Vector of x-coordinates
 - `ys`: corresponding vector of y-coordinates
 - `rangeout`: 0 (default) for phase values in [0, 2π); 1 for phase values in (-π, π].
--`unwrap`: set to `true` to get "unwrapped" phases (default).
--`s`: set to `true` to additionally return the cummulative arclength traversed at each point (set `false` by default). 
+- `unwrap`: set to `true` to get "unwrapped" phases (default).
+- `s`: set to `true` to additionally return the cummulative arclength traversed at each point (set `false` by default). 
 
 # Reference
 [1] McConnell, Ross, et al. "psi-s correlation and dynamic time warping: two methods for tracking ice floes in SAR images." IEEE Transactions on Geoscience and Remote sensing 29.6 (1991): 1004-1012.
 
 # Example
 
-The example below builds a cardiod and obtains its ψ-s curve.
+The example below builds a cardioid and obtains its ψ-s curve.
 
 ```jldoctest; setup = :(using IceFloeTracker, Plots)
 julia> t = range(0,2pi,201);
@@ -99,34 +100,29 @@ julia> psi = make_psi_s(x,y)
  9.351474515010644
  9.393362034233482
 
- julia> plot(t[1:end-1], psi) # inspect psi-s curve -- should be a straigth line from (0,0) to (2π, 3π)
+ julia> plot(t[1:end-1], psi) # inspect psi-s curve -- should be a straigth line from (0, 0) to (2π, 3π)
 ```
 """
-function make_psi_s(xs::Vector{Float64},
-                    ys::Vector{Float64};rangeout::Int64=0,
-                    unwrap::Bool=true,
-                    s::Bool=false)
-    @assert length(xs) == length(ys) "Vectors `xs` and `ys` must have the same size."
-    @assert 0<= rangeout <= 1 "Invalid value for `rangeout`($rangeout). Choose `rangeout=0` for an phase output in [0, 2π) or `rangeout=1` for (-π, π]."
-
+function make_psi_s(x::Vector{<:Number},
+                    y::Vector{<:Number};rangeout::Bool=true,
+                    unwrap::Bool=true)
     # gradient
-        dx = xs[2:end] - xs[1:end-1]
-        dy = ys[2:end] - ys[1:end-1]
-
+        dx, dy = grad(x, y)
+        
     # get phase curve
-        if rangeout == 0
-            phase = atan2(dy, dx)
-        elseif rangeout == 1
+        if rangeout
+            phase = atan2.(dy, dx)
+        else
             phase = atan.(dy, dx)
         end
 
         if unwrap
             phase = DSP.unwrap(phase)
         end
+    
+    # compute arclength
+        s = cumsum([norm(collect(v_i)) for v_i in eachrow([dx dy])])
+    
+    return phase, s
 
-        if s
-            return [phase]
-        end
-        
-        return phase
 end
