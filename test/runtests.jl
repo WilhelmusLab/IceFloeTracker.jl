fbfa01bb0ee58af89395706c722ae340374decf8
Carlos Paniagua <cpaniaguam@gmail.com>
2022-10-05 16:31:35 -0400

    fix: commented line

--- test/runtests.jl
+++ test/runtests.jl
@@ -52,7 +52,7 @@ to_test = alltests # uncomment this line to run all tests or add individual file
 # "test-regionprops.jl",
 # "test-psi-s.jl",
 # "test-crosscorr.jl",
-# ]
+]
 
 # Run the tests
 @testset "IceFloeTracker.jl" begin
