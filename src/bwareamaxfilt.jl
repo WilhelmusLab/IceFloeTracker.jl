9784d92333e60746cd7454d4c46d298d38e82c17
Carlos Paniagua <cpaniaguam@gmail.com>
2022-10-17 13:35:02 -0400

    fix: add return type to funcs etc

--- src/bwareamaxfilt.jl
+++ src/bwareamaxfilt.jl
@@ -3,13 +3,13 @@
 
 Get the label `k` in dictionary `d` for which d[k] is maximal.
 """
-function get_max_label(d::Dict{Int, Int})
-    drev = Dict{Int,Int}()
+function get_max_label(d::Dict{Int, Int})::Int
+    drev = Dict{Int, Int}()
     mx = 0
-    for (k,v) in d
-        drev[v] = k
-        if v > mx
-            mx = v
+    for (k, value) in d
+        drev[value] = k
+        if value > mx
+            mx = value
         end
     end
     return drev[mx]
@@ -22,10 +22,10 @@ Make 0 all values in `labeled_arr` that are not equal to `label`.
 
 See also [`filt_except_label!`](@ref) 
 """
-function filt_except_label(labeled_arr::Array{Int64, 2}, label::Int64)
+function filt_except_label(labeled_arr::Array{Int64, 2}, label::Int64)::Array{Int64, 2}
     outlabel = similar(labeled_arr)
-    for (i, v) in enumerate(labeled_arr)
-        v != label ? outlabel[i] = 0 : outlabel[i] = label
+    for (i, value) in enumerate(labeled_arr)
+        value != label ? outlabel[i] = 0 : outlabel[i] = label
     end
     return outlabel
 end
@@ -37,9 +37,9 @@ In-place version of `filt_except_label`.
 
 See also [`filt_except_label`](@ref) 
 """
-function filt_except_label!(labeled_arr::Array{Int64, 2}, label::Int64)
-    for (i, v) in enumerate(labeled_arr)
-        v != label ? labeled_arr[i] = 0 : labeled_arr[i] = label
+function filt_except_label!(labeled_arr::Array{Int64, 2}, label::Int64)::Array{Int64, 2}
+    for (i, value) in enumerate(labeled_arr)
+        value != label ? labeled_arr[i] = 0 : labeled_arr[i] = label
     end
     return labeled_arr
 end
@@ -84,8 +84,8 @@ function bwareamaxfilt!(bwimg::AbstractArray{Bool})::BitMatrix
     d = get_areas(label)
     mx_label = get_max_label(d)
     label = (0 .!= filt_except_label!(label,mx_label))
-    for (i,v) in enumerate(bwimg)
-        bwimg[i] = (v && label[i])
+    for (i, value) in enumerate(bwimg)
+        bwimg[i] = (value && label[i])
     end
     return bwimg
 end
