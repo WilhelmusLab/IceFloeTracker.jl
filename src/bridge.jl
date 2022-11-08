45342176f1aeb8dca090100ba5481cd7cee2ddb2
Carlos Paniagua <cpaniaguam@gmail.com>
2022-11-08 18:33:23 -0500

    feat: bridge
    - Add CI tests
    - Add type annotations

--- src/bridge.jl
+++ src/bridge.jl
@@ -1,3 +1,5 @@
+include("./lut/lutbridge.jl")
+
 """
     _bin9todec(v)
 
@@ -12,28 +14,31 @@ julia> _bin9todec([1 1 1 1 1 1 1 1 1])
 511
 ```
 """
-function _bin9todec(v)
+function _bin9todec(v::AbstractArray)::Int64
     return sum(vec(v) .* 2 .^ (0:length(v)-1))
 end
 
-function operator_lut(I, img, nhood, lut) # for bridge
+function _operator_lut(I::CartesianIndex{2}, img::AbstractArray{Bool},
+    nhood::CartesianIndices{2, Tuple{UnitRange{Int64}, UnitRange{Int64}}},
+    lut::Vector{T})::T where T # for bridge
     # corner pixels
     if length(nhood) == 4
-        return false
-    elseif length(nhood) == 6
+        return false # false for bridge and some other operations like hbreak, branch
+    elseif length(nhood) == 6 # edge pixels
         filled = padnhood(img, I, nhood)
-    else
+    else # interior pixels
         filled = img[nhood]
     end
     return lut[_bin9todec(filled)+1]
 end
 
-function bridge_operator_lut(I, img, nhood)
-    lutbridge = vec(readdlm("src/lut/lutbridge.csv", ',', Bool))
-    return operator_lut(I, img, nhood, lutbridge)
+function _bridge_operator_lut(I::CartesianIndex{2}, img::AbstractArray{Bool},
+    nhood::CartesianIndices{2, Tuple{UnitRange{Int64}, UnitRange{Int64}}})
+    lutbridge = make_lutbridge()
+    return _operator_lut(I, img, nhood, lutbridge)
 end
 
-function bridge_filter(img, operator)
+function _bridge_filter(img::T, operator::Function)::T where T<:AbstractArray{Bool}
     out = zeros(Bool,size(img))
     R = CartesianIndices(img)
     I_first, I_last = first(R), last(R)
@@ -47,9 +52,61 @@ function bridge_filter(img, operator)
     return out .|| img
 end
 
-function bridge(bw)
-    if eltype(bw) != Bool
-        bw = Bool.(bw)
-    end
-    bridge_filter(bw, bridge_operator_lut)
+"""
+    bridge(bw)
+
+Set 0-valued pixels to 1 if they have two nonzero neighbors that are not connected. Note the following exceptions:
+
+0 0 0           0 0 0
+1 0 1  becomes  1 1 1
+0 0 0           0 0 0
+
+1 0 1           1 1 1
+0 0 0  becomes  0 0 0
+0 0 0           0 0 0
+
+The same applies to all their corresponding rotations.
+
+# Examples
+
+```jldoctest; setup = :(using IceFloeTracker)
+julia> bw = [0 0 0; 0 0 0; 1 0 1]
+3×3 Matrix{Int64}:
+ 0  0  0
+ 0  0  0
+ 1  0  1
+
+julia> bridge(bw)
+3×3 BitMatrix:
+ 0  0  0
+ 0  0  0
+ 1  1  1
+
+julia> bw = [1 0 0; 1 0 1; 0 0 1]
+3×3 Matrix{Int64}:
+ 1  0  0
+ 1  0  1
+ 0  0  1
+
+julia> bridge(bw)
+3×3 BitMatrix:
+ 1  1  0
+ 1  1  1
+ 0  1  1
+
+ julia> bw = [1 0 1; 0 0 0; 1 0 1]
+3×3 Matrix{Int64}:
+ 1  0  1
+ 0  0  0
+ 1  0  1
+
+julia> bridge(bw)
+3×3 BitMatrix:
+ 1  1  1
+ 1  1  1
+ 1  1  1
+```
+"""
+function bridge(bw::T)::T where T<:AbstractArray{Bool}
+    _bridge_filter(bw, _bridge_operator_lut)
 end
---
+++ test/test-bridge.jl
@@ -0,0 +1,5 @@
+@testset "bridge tests" begin
+    bwin = readdlm("./test_inputs/bridge/bridge_in.csv", ',', Bool)
+    bwexpected = readdlm("./test_inputs/bridge/bridge_expected.csv", ',', Bool)
+    @test bridge(bwin) == bwexpected
+end
