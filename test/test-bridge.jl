a91ec7a964e53489068155d67e51ecb7327f26c7
Carlos Paniagua <cpaniaguam@gmail.com>
2022-11-09 10:55:06 -0500

    fix: add header to file for display in runtime

--- test/test-bridge.jl
+++ test/test-bridge.jl
@@ -1,4 +1,6 @@
 @testset "bridge tests" begin
+    println("-------------------------------------------------")
+    println("---------------- bridge tests -------------------")
     bwin = readdlm("./test_inputs/bridge/bridge_in.csv", ',', Bool)
     bwexpected = readdlm("./test_inputs/bridge/bridge_expected.csv", ',', Bool)
     @test bridge(bwin) == bwexpected
