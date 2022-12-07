c703ad860cf771256007c737bbb80bb531cb3673
Carlos Paniagua <cpaniaguam@gmail.com>
2022-12-07 13:33:13 -0500

    procs: prep cli for tasks

--- scripts/ice-floe-tracker.jl
+++ scripts/ice-floe-tracker.jl
@@ -1,6 +1,6 @@
 #!/usr/bin/env julia
 using Pkg
-Pkg.activate(@__DIR__)
+# Pkg.activate(@__DIR__)
 
 using ArgParse
 using IceFloeTracker
@@ -10,15 +10,27 @@ function main(args)
 
     @add_arg_table! settings begin
         "fetchdata"
-        help = "Fetches source data for ice floe tracking"
+        help = "Fetch source data for ice floe tracking"
         action = :command
 
-        "landmask"
-        help = "generates land mask images"
+        "prep"
+        help = "Preprocess input images"
         action = :command
 
-        "cloudmask"
-        help = "generates cloud mask images"
+        "seg"
+        help = "Do segmentation on preprocessed images"
+        action = :command
+
+        "fext"
+        help = "Do feature extraction on segmented images"
+        action = :command
+
+        "track"
+        help = "Do tracking procedure using extracted features from segmented images"
+        action = :command
+
+        "ltrack"
+        help = "Do long tracking procedure"
         action = :command
     end
 
@@ -28,16 +40,20 @@ function main(args)
         required = true
     end
 
+    # metadata requirements might change later
     command_common_args = [
         "metadata",
-        Dict(:help => "image metadata file", :required => true),
+        Dict(:help => "image metadata file", :required => false),
         "input",
         Dict(:help => "input image directory", :required => true),
         "output",
         Dict(:help => "output image directory", :required => true),
     ]
-    add_arg_table!(settings["landmask"], command_common_args...)
-    add_arg_table!(settings["cloudmask"], command_common_args...)
+
+    add_arg_table!(settings["prep"], command_common_args...)
+    add_arg_table!(settings["seg"], command_common_args...)
+    add_arg_table!(settings["fext"], command_common_args...)
+    add_arg_table!(settings["track"], command_common_args...)
 
     parsed_args = parse_args(args, settings; as_symbols=true)
 
