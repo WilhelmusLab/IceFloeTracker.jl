6ede22d8c73648cc0f62ca91ed0d922329d67c30
Timothy Divoll <timothy.divoll@gmail.com>
2022-12-09 09:05:06 -0500

    fix: update filename in config

--- test/test-segmentation-f.jl
+++ test/test-segmentation-f.jl
@@ -9,7 +9,7 @@
     landmask = convert(BitMatrix, load(current_landmask_file))
     watershed_intersect = load(watershed_test_file) .> 0.5
     ice_labels =
-        Int64.(vec(DelimitedFiles.readdlm("$(test_data_dir)/ice_labels_matlab.csv", ',')))
+        Int64.(vec(DelimitedFiles.readdlm("$(test_data_dir)/ice_labels_floe_region.csv", ',')))
     matlab_isolated_floes = convert(
         BitMatrix, load("$(test_data_dir)/matlab_isolated_floes.png")
     )
@@ -17,11 +17,11 @@
     ## Run function with Matlab inputs
 
     @time isolated_floes = IceFloeTracker.segmentation_F(
-        segmentation_C_ice_mask,
-        segmentation_B_not_ice_mask,
-        watershed_intersect,
-        cloudmask,
-        landmask,
+        segmentation_C_ice_mask[ice_floe_test_region...],
+        segmentation_B_not_ice_mask[ice_floe_test_region...],
+        watershed_intersect[ice_floe_test_region...],
+        cloudmask[ice_floe_test_region...],
+        landmask[ice_floe_test_region...],
         ice_labels,
     )
 
@@ -32,5 +32,5 @@
     IceFloeTracker.@persist isolated_floes isolated_floes_filename
 
     @test typeof(isolated_floes) == typeof(matlab_isolated_floes)
-    @test test_similarity(isolated_floes, matlab_isolated_floes, 0.07)
+    @test test_similarity(isolated_floes, matlab_isolated_floes[ice_floe_test_region...], 0.07)
 end
