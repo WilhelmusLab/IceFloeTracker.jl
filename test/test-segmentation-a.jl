2b8056b00860938e981ca2321572bc91d2459baa
Timothy Divoll <timothy.divoll@gmail.com>
2022-12-12 13:54:36 -0500

    test: update to use matlab ice_water_discrim input

--- test/test-segmentation-a.jl
+++ test/test-segmentation-a.jl
@@ -3,7 +3,7 @@
     println("------------------------------------------------")
     println("------------ Create Segmentation-A Test --------------")
 
-    ice_water_discriminated_image = float64.(load(ice_water_discrim_test_file))
+    ice_water_discriminated_image = float64.(load("$(test_data_dir)/matlab_ice_water_discrim.png"))
     cloudmask = convert(BitMatrix, load(cloudmask_test_file))
     ice_labels = DelimitedFiles.readdlm("$(test_data_dir)/ice_labels_julia.csv", ',')
     ice_labels = Int64.(vec(ice_labels))
@@ -35,8 +35,8 @@
     IceFloeTracker.@persist segmented_ice segmented_ice_filename
 
     @test typeof(segmented_A) == typeof(matlab_segmented_A_bitmatrix)
-    @test test_similarity(matlab_segmented_A_bitmatrix, segmented_A, 0.077)
+    @test test_similarity(matlab_segmented_A_bitmatrix, segmented_A, 0.005)
 
     @test typeof(segmented_ice) == typeof(matlab_segmented_ice)
-    @test test_similarity(matlab_segmented_ice, segmented_ice, 0.077)
+    @test test_similarity(matlab_segmented_ice, segmented_ice, 0.001)
 end
