95a6fdc7bac830c7fd2b66988be3d51371a58b7e
Carlos Paniagua <cpaniaguam@gmail.com>
2022-12-20 12:52:56 -0500

    cli: use imperative mood for help lines

--- scripts/ice-floe-tracker.jl
+++ scripts/ice-floe-tracker.jl
@@ -10,38 +10,38 @@ function main(args)
 
     @add_arg_table! settings begin
         "fetchdata"
-        help = "Fetches source data for ice floe tracking"
+        help = "Fetch source data for ice floe tracking"
         action = :command
 
         "landmask"
-        help = "generates land mask images"
+        help = "Generate land mask images"
         action = :command
 
         "cloudmask"
-        help = "generates cloud mask images"
+        help = "Generate cloud mask images"
         action = :command
     end
 
     @add_arg_table! settings["fetchdata"] begin
         "output"
-        help = "output image directory"
+        help = "Output image directory"
         required = true
     end
 
     landmask_args = [
         "input",
-        Dict(:help => "input image directory", :required => true),
+        Dict(:help => "Input image directory", :required => true),
         "output",
-        Dict(:help => "output image directory", :required => true),
+        Dict(:help => "Output image directory", :required => true),
     ]
 
     command_common_args = [
         "metadata",
-        Dict(:help => "image metadata file", :required => true),
+        Dict(:help => "Image metadata file", :required => true),
         "input",
-        Dict(:help => "input image directory", :required => true),
+        Dict(:help => "Input image directory", :required => true),
         "output",
-        Dict(:help => "output image directory", :required => true),
+        Dict(:help => "Output image directory", :required => true),
     ]
 
     add_arg_table!(settings["landmask"], landmask_args...)
