e6ddf3bdd6038866224b808a4ac31895c275a861
cpaniaguam <68481491+cpaniaguam@users.noreply.github.com>
2023-02-06 22:38:19 -0500

    fix: regionprops_table for pipeline - type stability - zero indexing from skimage for bounding boxes - add default property vector

--- test/test-regionprops.jl
+++ test/test-regionprops.jl
@@ -4,23 +4,25 @@
 
     Random.seed!(123)
     bw_img = rand([0, 1], 5, 10)
-    label_img = Images.label_components(bw_img, trues(3, 3))
-    properties = ["area", "perimeter", "area_convex", "solidity"]
+    label_img = IceFloeTracker.label_components(bw_img, trues(3, 3))
+    properties = ("centroid", "area", "major_axis_length", "minor_axis_length", "convex_area", "bbox")
     extra_props = nothing
     table = IceFloeTracker.regionprops_table(
-        label_img, bw_img; properties=properties, dataframe=true
+        label_img, bw_img; properties=properties
     )
     total_labels = maximum(label_img)
 
     # Tests for regionprops_table
 
     @test typeof(table) <: DataFrame && # check correct data type
-        sort(names(table)) == sort(properties) && # check correct set of properties
-        size(table) == (total_labels, length(properties)) # check correct table size
+          4 == [p in names(table) for p in properties] |> sum && # check correct set of properties
+          size(table) == (total_labels, length(properties) + 4) # check correct table size
 
-    dict = IceFloeTracker.regionprops_table(label_img, bw_img; properties=properties)
-    @test typeof(dict) <: Dict && # check correct data type
-        sort(collect(keys(dict))) == sort(properties) #  check correct size and properties
+    # Check no value in columns bbox-* of table is 0 (zero indexing from skimage)
+    @test Matrix(table[:, ["bbox-0", "bbox-1", "bbox-2", "bbox-3"]] .> 0) |> all
+
+    # check default properties
+    @test table == IceFloeTracker.regionprops_table(label_img) 
 
     # Tests for regionprops
 
@@ -29,4 +31,4 @@
     # Check some data matches in table
     randnum = rand(1:total_labels)
     @test table.area[randnum] == regions[randnum].area
-end;
+end
