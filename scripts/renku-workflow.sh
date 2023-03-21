b91d92f3112a210d0246a8f4a87ed80579159d88
Timothy Divoll <timothy.divoll@gmail.com>
2023-03-21 15:45:20 -0400

    feat:add renku pseudocode

--- scripts/renku-workflow.sh
+++ scripts/renku-workflow.sh
@@ -1,4 +1,17 @@
+## Add parameters to Renku knowledgebase
+echo '{"@startdate": "2023-02-09",
+       "@enddate": "2023-02-12",
+       "@bounding-box": "81 -23 79 12",
+       "@main-outdir": "output",
+       "@fetchdata-outdir": "output/data",
+       "@landmask-outdir": "output/landmasks",
+       "@soit-outdir": "output/soit",
+       "@centroid-x": "72",
+       "@centroid-y": "-18"
+}' > parameters.json
+
 ## Fetch data
+mkdir -p 
 
 ## Satellite Overpass Identification Tool
 
