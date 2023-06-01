aeba4ab454aa422c616b931e8494d12dbca84f36
Carlos Paniagua <cpaniaguam@gmail.com>
2023-06-01 15:32:27 -0400

    fix: test/test-matchcorr

--- test/test-matchcorr.jl
+++ test/test-matchcorr.jl
@@ -6,9 +6,9 @@
             deserialize.([
                 joinpath(path, f) for f in ["f1.dat", "f2.dat", "f3.dat", "f4.dat"]
             ])
-        mm, c = matchcorr(floes[1], floes[2], 400)
+        mm, c = matchcorr(floes[1], floes[2], 400.0)
         @test isapprox(mm, 0.0; atol=0.05) && isapprox(c, 0.99; atol=0.05)
-        @test all(isnan.(collect(matchcorr(floes[3], floes[4], 400))))
+        @test all(isnan.(collect(matchcorr(floes[3], floes[4], 400.0))))
     end
 
     @testset "tracker" begin
