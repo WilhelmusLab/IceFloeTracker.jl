c58ae72b33a9784fd351f0b55105d6494ea4ce2c
Carlos Paniagua <cpaniaguam@gmail.com>
2023-08-29 15:45:42 -0400

    my commit

--- src/bwperim.jl
+++ src/bwperim.jl
@@ -1,15 +1,3 @@
-"""
-    bwperim(bwimg)
-
-Locate the pixels at the boundary of objects in an binary image `bwimg` using 8-pixel connectivity.
-
-# Arguments
-- `bwimg`: Binary (black/white -- 1/0) image
-
-# Examples
-
-```jldoctest; setup = :(using IceFloeTracker)
-julia> A = zeros(Bool, 13, 16); A[2:6, 2:6] .= 1; A[4:8, 7:10] .= 1; A[10:12,13:15] .= 1; A[10:12,3:6] .= 1;
 
 julia> A
 13×16 Matrix{Bool}:
