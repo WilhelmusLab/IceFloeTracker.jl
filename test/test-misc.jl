dc1d5396417497a28dcecf5118a931904109a537
Carlos Paniagua <cpaniaguam@gmail.com>
2024-04-02 20:03:29 +0000

    chore: update version nums

--- test/test-misc.jl
+++ test/test-misc.jl
@@ -1,3 +1,3 @@
 println("------------------------------------------------
 ----------------- Misc. Tests ------------------")
-@test IFTVERSION == VersionNumber(0, 4, 0)
+@test IFTVERSION == VersionNumber(0, 4, 3)
