2b13ead87be37018e37ea29bdd5abd7f1b006137
Carlos Paniagua <cpaniaguam@gmail.com>
2024-04-03 12:51:56 -0400

    feat: get IFTVERSION from Project.toml

--- test/test-misc.jl
+++ test/test-misc.jl
@@ -1,3 +1,10 @@
 println("------------------------------------------------
 ----------------- Misc. Tests ------------------")
-@test IFTVERSION == VersionNumber(0, 4, 3)
+
+
+# Define a test to check the version number of the package matches the version number in the Project.toml file. Use the get_version_from_toml function to get the version number from the Project.toml file. The version number is then compared to the version number of the package. If the version numbers match, the test passes. If the version numbers do not match, the test fails.
+
+@testset "Version number" begin
+    @test IceFloeTracker.get_version_from_toml(dirname((@__DIR__))) == IFTVERSION
+end
+
