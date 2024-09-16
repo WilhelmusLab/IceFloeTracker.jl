129b44ce6c30ab15d529366369256f7b2ac93ed7
Carlos Paniagua <cpaniaguam@gmail.com>
2024-09-16 02:14:54 -0400

    refactor: rename reference image parameter to false_color_image for consistency

--- src/cloudmask.jl
+++ src/cloudmask.jl
@@ -1,10 +1,10 @@
-function convert_to_255_matrix(img)
+function convert_to_255_matrix(img)::Matrix{Int}
     img_clamped = clamp.(img, 0.0, 1.0)
     return round.(Int, img_clamped * 255)
 end
 
 function _get_masks(
-    ref_image::Matrix{RGB{Float64}};
+    false_color_image::Matrix{RGB{Float64}};
     prelim_threshold::Float64=Float64(110 / 255),
     band_7_threshold::Float64=Float64(200 / 255),
     band_2_threshold::Float64=Float64(190 / 255),
@@ -13,14 +13,14 @@ function _get_masks(
     use_uint8::Bool=false,
 )::Tuple{BitMatrix,BitMatrix}
 
-    ref_view = channelview(ref_image)
-    ref_image_b7 = @view ref_view[1, :, :]
+    ref_view = channelview(false_color_image)
+    false_color_image_b7 = @view ref_view[1, :, :]
     if use_uint8
-        ref_image_b7 = convert_to_255_matrix(ref_image_b7)
+        false_color_image_b7 = convert_to_255_matrix(false_color_image_b7)
     end
 
-    clouds_view = ref_image_b7 .> prelim_threshold
-    mask_b7 = ref_image_b7 .< band_7_threshold
+    clouds_view = false_color_image_b7 .> prelim_threshold
+    mask_b7 = false_color_image_b7 .< band_7_threshold
     mask_b2 = @view(ref_view[2, :, :])
     if use_uint8
         mask_b2 = convert_to_255_matrix(mask_b2)
@@ -33,7 +33,7 @@ function _get_masks(
     mask_b7b2 = mask_b7 .&& mask_b2
 
     # Next find pixels that meet both thresholds and mask them from band 7 (channel 1) and band 2 (channel 2)
-    b7_masked = mask_b7b2 .* ref_image_b7
+    b7_masked = mask_b7b2 .* false_color_image_b7
 
     _b2 = @view(ref_view[2, :, :])
     b2_masked = use_uint8 ? convert_to_255_matrix(_b2) : _b2
@@ -47,12 +47,12 @@ end
 
 
 """
-    create_cloudmask(falsecolor_image; prelim_threshold, band_7_threshold, band_2_threshold, ratio_lower, ratio_upper)
+    create_cloudmask(false_color_image; prelim_threshold, band_7_threshold, band_2_threshold, ratio_lower, ratio_upper)
 
 Convert a 3-channel false color reflectance image to a 1-channel binary matrix; clouds = 0, else = 1. Default thresholds are defined in the published Ice Floe Tracker article: Remote Sensing of the Environment 234 (2019) 111406.
 
 # Arguments
-- `falsecolor_image`: corrected reflectance false color image - bands [7,2,1]
+- `false_color_image`: corrected reflectance false color image - bands [7,2,1]
 - `prelim_threshold`: threshold value used to identify clouds in band 7, N0f8(RGB intensity/255)
 - `band_7_threshold`: threshold value used to identify cloud-ice in band 7, N0f8(RGB intensity/255)
 - `band_2_threshold`: threshold value used to identify cloud-ice in band 2, N0f8(RGB intensity/255)
@@ -61,7 +61,7 @@ Convert a 3-channel false color reflectance image to a 1-channel binary matrix; 
 
 """
 function create_cloudmask(
-    ref_image::Matrix{RGB{Float64}};
+    false_color_image::Matrix{RGB{Float64}};
     prelim_threshold::Float64=Float64(110 / 255),
     band_7_threshold::Float64=Float64(200 / 255),
     band_2_threshold::Float64=Float64(190 / 255),
@@ -69,7 +69,7 @@ function create_cloudmask(
     ratio_upper::Float64=0.75,
 )::BitMatrix
     mask_cloud_ice, clouds_view = _get_masks(
-        ref_image,
+        false_color_image,
         prelim_threshold=prelim_threshold,
         band_7_threshold=band_7_threshold,
         band_2_threshold=band_2_threshold,
@@ -83,19 +83,19 @@ function create_cloudmask(
 end
 
 """
-    apply_cloudmask(ref_image, cloudmask)
+    apply_cloudmask(false_color_image, cloudmask)
 
 Zero out pixels containing clouds where clouds and ice are not discernable. Arguments should be of the same size.
 
 # Arguments
-- `ref_image`: reference image, e.g. corrected reflectance false color image bands [7,2,1] or grayscale
+- `false_color_image`: reference image, e.g. corrected reflectance false color image bands [7,2,1] or grayscale
 - `cloudmask`: binary cloudmask with clouds = 0, else = 1
 
 """
 function apply_cloudmask(
-    ref_image::Matrix{RGB{Float64}}, cloudmask::AbstractArray{Bool}
+    false_color_image::Matrix{RGB{Float64}}, cloudmask::AbstractArray{Bool}
 )::Matrix{RGB{Float64}}
-    masked_image = cloudmask .* ref_image
+    masked_image = cloudmask .* false_color_image
     image_view = channelview(masked_image)
     cloudmasked_view = StackedView(
         zeroarray, @view(image_view[2, :, :]), @view(image_view[3, :, :])
@@ -105,14 +105,14 @@ function apply_cloudmask(
 end
 
 function apply_cloudmask(
-    ref_image::Matrix{Gray{Float64}}, cloudmask::AbstractArray{Bool}
+    false_color_image::Matrix{Gray{Float64}}, cloudmask::AbstractArray{Bool}
 )::Matrix{Gray{Float64}}
-    return Gray.(cloudmask .* ref_image)
+    return Gray.(cloudmask .* false_color_image)
 end
 
 function create_clouds_channel(
-    cloudmask::AbstractArray{Bool}, ref_image::Matrix{RGB{Float64}}
+    cloudmask::AbstractArray{Bool}, false_color_image::Matrix{RGB{Float64}}
 )::Matrix{Gray{Float64}}
-    return Gray.(@view(channelview(cloudmask .* ref_image)[1, :, :]))
+    return Gray.(@view(channelview(cloudmask .* false_color_image)[1, :, :]))
 end
 
