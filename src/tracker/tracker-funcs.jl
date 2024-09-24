9ee48331bff72c6224ac7d5fec40e6c09c2a3f70
John Gerrard Holland <john_holland1@brown.edu>
2024-09-24 14:11:34 +0000

    add support for float centroids in convertcentroid

--- src/tracker/tracker-funcs.jl
+++ src/tracker/tracker-funcs.jl
@@ -508,13 +508,13 @@ Convert the centroid coordinates from row and column to latitude and longitude d
 """
 function convertcentroid!(propdf, latlondata, colstodrop)
     latitude, longitude = [
-        [latlondata[c][x, y] for
+        [latlondata[c][Integer(round(x)), Integer(round(y))] for
          (x, y) in zip(propdf.row_centroid, propdf.col_centroid)] for
         c in ["latitude", "longitude"]
     ]
 
     x, y = [
-        [latlondata[c][z] for z in V] for
+        [latlondata[c][Integer(round(z))] for z in V] for
         (c, V) in zip(["Y", "X"], [propdf.row_centroid, propdf.col_centroid])
     ]
 
