7f7dc84b3a08d7b7a20823efe51a924caef55591
Carlos Paniagua <cpaniaguam@gmail.com>
2024-10-19 01:06:48 -0400

    test: add test for reconstruct_erosion

--- test/test-reconstruct.jl
+++ test/test-reconstruct.jl
@@ -20,4 +20,9 @@ close(r)
         coins_opened = open_by_reconstruction(coins, se_disk1)
         @test sum(coins_opened) == 7552396
     end
+
+    @testset "reconstruct_erosion" begin
+        coins_r_erosion = reconstruct_erosion(coins, se_disk1)
+        @test sum(coins_r_erosion) == 11179481
+    end
 end
