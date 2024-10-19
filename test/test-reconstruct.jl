65e8100d83b5e828b187211bd7ff42f4c76be057
Carlos Paniagua <cpaniaguam@gmail.com>
2024-10-19 00:57:03 -0400

    test: open_by_reconstruction

--- test/test-reconstruct.jl
+++ test/test-reconstruct.jl
@@ -3,11 +3,21 @@ using ZipFile # delete later
 using DelimitedFiles: readdlm
 using IceFloeTracker: imcomplement
 
-@testset "imcomplement" begin
-    r = ZipFile.Reader("test_inputs/coins.zip")
-    coins = readdlm(r.files[1], ',', Int)
-    close(r)
-    @test true
+r = ZipFile.Reader("test/test_inputs/coins.zip")
+coins = readdlm(r.files[1], ',', Int)
+close(r)
 
+@testset "reconstruct" begin
+    @testset "imcomplement" begin
+        @test imcomplement(coins) == 255 .- coins
 
+        coins_gray = Gray.(coins ./ 255)
+        @test imcomplement(coins_gray) == 1 .- coins_gray
+    end
+
+    @testset "open_by_reconstruction" begin
+        se_disk1 = IceFloeTracker.MorphSE.StructuringElements.strel_diamond((3, 3))
+        coins_opened = open_by_reconstruction(coins, se_disk1)
+        @test sum(coins_opened) == 7552396
+    end
 end
