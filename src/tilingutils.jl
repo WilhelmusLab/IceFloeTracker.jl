7c186354eccf4ee9520d0a538b8182a5562a134d
Carlos Paniagua <cpaniaguam@gmail.com>
2024-10-19 22:08:20 -0400

    feat: add documentation for imbrighten

--- src/tilingutils.jl
+++ src/tilingutils.jl
@@ -225,12 +225,22 @@ function get_brighten_mask(equalized_gray_reconstructed_img, gamma_green)
     return to_uint8(equalized_gray_reconstructed_img - gamma_green)
 end
 
+"""
+    imbrighten(img, brighten_mask, bright_factor)
 
+Brighten the image using a mask and a brightening factor.
 
+# Arguments
+- `img`: The input image.
+- `brighten_mask`: A mask indicating the pixels to brighten.
+- `bright_factor`: The factor by which to brighten the pixels.
+
+# Returns
+- The brightened image.
+"""
 function imbrighten(img, brighten_mask, bright_factor)
-    img = Float64.(img);
+    img = Float64.(img)
     brighten_mask = brighten_mask .> 0
     img[brighten_mask] .= img[brighten_mask] * bright_factor
-    img = to_uint8(img)
+    return img = to_uint8(img)
 end
-
