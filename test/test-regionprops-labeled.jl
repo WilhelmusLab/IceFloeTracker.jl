8dc5597394693e0c29ec49ab00f58f70abe0ff92
John Gerrard Holland <john_holland1@brown.edu>
2024-10-24 19:09:48 +0000

    remove repetition in testing code

--- test/test-regionprops-labeled.jl
+++ test/test-regionprops-labeled.jl
@@ -27,11 +27,13 @@
 
     @info "testing props with label"
     props_with_label = DataFrame(label=Int[], min_row=Int[], min_col=Int[], max_row=Int[], max_col=Int[])
-    push!(props_with_label, (1, 1, 1, 4, 4))
-    push!(props_with_label, (2, 1, 4, 5, 8))
-    push!(props_with_label, (3, 1, 8, 6, 13))
-    push!(props_with_label, (4, 5, 1, 10, 7))
-    push!(props_with_label, (5, 5, 1, 10, 7))
+    push!(props_with_label, 
+        (1, 1, 1, 4, 4), 
+        (2, 1, 4, 5, 8), 
+        (3, 1, 8, 6, 13), 
+        (4, 5, 1, 10, 7), 
+        (5, 5, 1, 10, 7)
+    )
 
 
     @test cropfloe(img1, props_with_label, 1) == [
@@ -78,11 +80,13 @@
 
     @info "testing props without label"
     props_without_label = DataFrame(min_row=Int[], min_col=Int[], max_row=Int[], max_col=Int[])
-    push!(props_without_label, (1, 1, 4, 4))
-    push!(props_without_label, (1, 4, 5, 8))
-    push!(props_without_label, (1, 8, 6, 13))
-    push!(props_without_label, (5, 1, 10, 7))
-    push!(props_without_label, (5, 1, 10, 7))
+    push!(props_without_label, 
+        (1, 1, 4, 4),
+        (1, 4, 5, 8),
+        (1, 8, 6, 13),
+        (5, 1, 10, 7),
+        (5, 1, 10, 7)
+    ) 
 
     @test cropfloe(img1, props_without_label, 1) == [
         0 0 0 0
