9232ad941742cd4f0b44630819e867868b5ac067
Carlos Paniagua <cpaniaguam@gmail.com>
2024-10-29 00:15:42 -0400

    test: add test for morph_fill functionality

---
+++ test/test-morph-fill.jl
@@ -0,0 +1,20 @@
+@testset "morp_fill.jl" begin
+    println("------------------------------------------------")
+    println("----------- Create Morp Fill Test --------------")
+
+    using IceFloeTracker: morph_fill
+    img = Bool[
+        0 0 0 0 0
+        0 1 1 1 0
+        0 1 0 1 0
+        0 1 1 1 0
+        0 0 0 0 0
+    ]
+
+    r = rand(1:100)
+
+    imgbig = repeat(img, r, r)
+
+    filled = morph_fill(imgbig)
+    @test sum(filled) - sum(imgbig) == r^2
+end
