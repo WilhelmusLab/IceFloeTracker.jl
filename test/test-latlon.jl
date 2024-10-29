a82f82ce74108d440ea12ac7d9d747988be071e6
cpaniaguam <cpaniaguam@gmail.com>
2024-10-29 10:40:19 -0400

    test: X, Y

--- test/test-latlon.jl
+++ test/test-latlon.jl
@@ -2,10 +2,17 @@ using IceFloeTracker: getlatlon
 
 @testset "getlatlon" begin
     imgpth = "test_inputs/latlon/latlon_test_image-2020-06-21T00_00_00Z.tif"
+
+    # vec needed to convert to vector instead of nx1 matrix
+    expected_X = vec(readdlm("test_inputs/latlon/X.csv", ',', Float64))
+    expected_Y = vec(readdlm("test_inputs/latlon/Y.csv", ',', Float64))
+
     expected_lat = readdlm("test_inputs/latlon/latitude.csv", ',', Float64)
     expected_lon = readdlm("test_inputs/latlon/longitude.csv", ',', Float64)
-
+    
     latlon = getlatlon(imgpth)
+    @test expected_X == latlon["X"]
+    @test expected_Y == latlon["Y"]
     @test expected_lat == latlon["latitude"]
     @test expected_lon == latlon["longitude"]
 end
