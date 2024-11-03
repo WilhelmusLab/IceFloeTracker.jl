77b4335685cc59cb20bd0a166458eba716e1a2c7
Carlos Paniagua <cpaniaguam@gmail.com>
2024-11-03 00:51:33 -0400

    refactor: update _generate_se! and streamline structuring element functions

--- src/special_strels.jl
+++ src/special_strels.jl
@@ -1,27 +1,31 @@
+"""
+    _generate_se!(se)
+
+Generate a structuring element by leveraging symmetry (mirroring and inverting) a given initial structuring element.
+"""
 function _generate_se!(se)
     se .= se .| reverse(se; dims=1)
     se .= se .| reverse(se; dims=2)
+    se .= .!se
     return nothing
 end
 
-se_disk50() = begin
+function se_disk50()
     se = [sum(c.I) <= 29 for c in CartesianIndices((99, 99))]
     _generate_se!(se)
-    se .= .!se
-    se
+    return se
 end
 
 make_landmask_se = se_disk50
 
-se_disk4() = begin
+function se_disk4()
     se = zeros(Bool, 7, 7)
     se[4, 4] = 1
-    bwdist(se) .<= 3.6
+    return bwdist(se) .<= 3.6
 end
 
-se_disk20() = begin
+function se_disk20()
     se = [sum(c.I) <= 11 for c in CartesianIndices((39, 39))]
     _generate_se!(se)
-    se .= .!se
-    se
+    return se
 end
