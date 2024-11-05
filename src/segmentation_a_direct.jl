8755d9cfb133dfa4de807d4ce487ff63b4289bb1
Carlos Paniagua <cpaniaguam@gmail.com>
2024-11-04 20:08:31 -0500

    feat: parallelize gradient magnitude

--- src/segmentation_a_direct.jl
+++ src/segmentation_a_direct.jl
@@ -138,7 +138,9 @@ Compute the gradient magnitude of an image using the Sobel operator.
 """
 function imgradientmag(img)
     h = centered([-1 0 1; -2 0 2; -1 0 1]')
-    Gx = imfilter(img, h', "replicate")
-    Gy = imfilter(img, h, "replicate")
+    Gx_future = Threads.@spawn IceFloeTracker.imfilter(img, h', "replicate")
+    Gy_future = Threads.@spawn IceFloeTracker.imfilter(img, h, "replicate")
+    Gx = fetch(Gx_future)
+    Gy = fetch(Gy_future)
     return hypot.(Gx, Gy)
 end
