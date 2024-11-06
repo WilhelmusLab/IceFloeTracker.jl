956054c6d14d2a7393cfc3450faf5b9286e8d21f
cpaniaguam <cpaniaguam@gmail.com>
2024-11-06 12:57:54 -0500

    feat: morph_fill

---
+++ src/morph_fill.jl
@@ -0,0 +1,38 @@
+include("./lut/lutfill.jl")
+
+function _fill_operator_lut(
+    I::CartesianIndex{2},
+    img::AbstractArray{Bool},
+    nhood::CartesianIndices{2,Tuple{UnitRange{Int64},UnitRange{Int64}}},
+)
+    return _operator_lut(I, img, nhood, make_lutfill())
+end
+
+"""
+    morph_fill(bw::T)::T where {T<:AbstractArray{Bool}}
+
+Fill holes in binary image `bw` by setting 0-valued pixels to 1 if they are surrounded by 1-valued pixels.
+
+# Examples
+
+```jldoctest; setup = :(using IceFloeTracker)
+julia> bw = Bool[
+        0 0 0 0 0
+        0 1 1 1 0
+        0 1 0 1 0
+        0 1 1 1 0
+        0 0 0 0 0
+    ];
+
+julia> morph_fill(bw)
+5×5 Matrix{Bool}:
+ 0  0  0  0  0
+ 0  1  1  1  0
+ 0  1  1  1  0
+ 0  1  1  1  0
+ 0  0  0  0  0
+"""
+function morph_fill(bw::T)::T where {T<:AbstractArray{Bool}}
+    # TODO: see about implemting _filter using parallelization
+    return _filter(bw, _fill_operator_lut)
+end
