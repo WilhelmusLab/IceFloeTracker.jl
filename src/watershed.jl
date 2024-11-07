18b3809168d424cf3780c9a2e4a027c3cfdc2733
Carlos Paniagua <cpaniaguam@gmail.com>
2024-11-06 20:30:23 -0500

    feat: implement watershed algorithms and add corresponding tests

---
+++ src/watershed.jl
@@ -0,0 +1,36 @@
+function watershed1(bw::T) where {T<:Union{BitMatrix,AbstractMatrix{Bool}}}
+    seg = -IceFloeTracker.bwdist(.!bw)
+    mask2 = imextendedmin(seg, 2)
+    seg = impose_minima(seg, mask2)
+    cc = label_components(imregionalmin(seg), trues(3, 3))
+    w = ImageSegmentation.watershed(seg, cc)
+    lmap = labels_map(w)
+    return Images.isboundary(lmap)
+end
+
+function watershed2(morph_residue, se=se_disk20())
+    # Task 1: Reconstruct morph_residue
+    task1 = Threads.@spawn begin
+        mr_reconst = reconstruct_erosion(morph_residue, se)
+        mr_reconst .= reconstruct(mr_reconst, se_disk20(), "dilation", true)
+        mr_reconst .= imcomplement(mr_reconst)
+        mr_reconst .= ImageMorphology.local_maxima(mr_reconst; connectivity=2) .> 0
+    end
+
+    # Task 2: Calculate gradient magnitude
+    task2 = Threads.@spawn begin
+        gmag = imgradientmag(histeq(morph_residue))
+    end
+
+    # Wait for both tasks to complete
+    mr_reconst = fetch(task1)
+    gmag = fetch(task2)
+
+    minimamarkers = Bool.(mr_reconst) .| segment_mask .| ice_mask
+    gmag .= impose_minima(gmag, minimamarkers)
+    cc = label_components(imregionalmin(gmag), trues(3, 3))
+    cc = label_components((gmag), trues(3, 3))
+    w = ImageSegmentation.watershed(morph_residue, cc)
+    lmap = labels_map(w)
+    return isboundary(lmap)
+end
