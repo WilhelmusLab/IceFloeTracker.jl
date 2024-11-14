26d26425eccb4bc861ed2382e5775cb34e606812
cpaniaguam <cpaniaguam@gmail.com>
2024-11-14 15:26:38 -0500

    refactor: reconstruct

--- src/reconstruction.jl
+++ src/reconstruction.jl
@@ -1,20 +1,32 @@
+"""
+    reconstruct(img, se, type, invert)
+
+Perform closing/opening by reconstruction on `img`.
+
+# Arguments
+- `img::AbstractArray`: The input image.
+- `se::AbstractArray`: The structuring element.
+- `type::String`: The type of morphological operation to perform. Must be either "dilation" (close by reconstruction) or `"erosion"` (open by reconstruction).
+- `invert::Bool=true`: Invert marker and mask before reconstruction.
+"""
 function reconstruct(img, se, type, invert::Bool=true)
     !(type == "dilation" || type == "erosion") &&
         throw(ArgumentError("Invalid type: $type. Must be 'dilation' or 'erosion'."))
 
-    type == "dilation" && (morphed = IceFloeTracker.MorphSE.dilate(img, se))
-    type == "erosion" &&
-        (morphed = IceFloeTracker.sk_morphology.erosion(img; footprint=collect(se)))
+    type == "dilation" && (
+        morphed = to_uint8(
+            IceFloeTracker.sk_morphology.dilation(img; footprint=collect(se))
+        )
+    )
+    type == "erosion" && (
+        morphed = to_uint8(IceFloeTracker.sk_morphology.erosion(img; footprint=collect(se)))
+    )
 
-    invert && (morphed = imcomplement(morphed); img = imcomplement(img))
+    invert && (morphed = imcomplement(to_uint8(morphed)); img = imcomplement(img))
 
     type == "dilation" && return IceFloeTracker.MorphSE.mreconstruct(
         IceFloeTracker.MorphSE.dilate, morphed, img
     )
 
     return IceFloeTracker.sk_morphology.reconstruction(morphed, img)
-end
-
-function reconstruct_erosion(img, se)
-    return reconstruct(img, se, "dilation")
 end
