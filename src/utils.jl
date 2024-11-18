9b43ac126c172044e65171b6265ba4191d388b26
Carlos Paniagua <cpaniaguam@gmail.com>
2024-11-18 11:46:47 -0500

    refactor: update imregionalmin function documentation and parameter name

--- src/utils.jl
+++ src/utils.jl
@@ -148,8 +148,19 @@ function impose_minima(
     return 1 .- sk_morphology.reconstruction(1 .- marker, 1 .- mask)
 end
 
-function imregionalmin(A, conn=2)
-    return ImageMorphology.local_minima(A; connectivity=conn) .> 0
+"""
+    imregionalmin(img, conn=2)
+
+Compute the regional minima of the image `img` using the connectivity `conn`.
+
+Returns a bitmatrix of the same size as `img` with the regional minima.
+
+# Arguments
+- `img`: Image object
+- `conn`: Neighborhood connectivity; in 2D, 1 = 4-neighborhood and 2 = 8-neighborhood
+"""
+function imregionalmin(img, conn=2)
+    return ImageMorphology.local_minima(img; connectivity=conn) .> 0
 end
 
 """
