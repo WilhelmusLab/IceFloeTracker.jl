405aaec89044ef6f4193d343b09d33171cf35811
Carlos Paniagua <cpaniaguam@gmail.com>
2024-11-20 12:50:59 -0500

    refactor: make coherent groups in preprocess)tiling

--- src/preprocess_tiling.jl
+++ src/preprocess_tiling.jl
@@ -86,8 +86,8 @@ function preprocess_tiling(
     prelim_icemask_params,
     brighten_factor,
 )
-    @info "Step 1/2: Get masks"
     begin
+        @debug "Step 1/2: Get masks"
         mask_cloud_ice, clouds_view = _get_masks(
             float64.(ref_image); ice_labels_thresholds...
         )
@@ -97,83 +97,114 @@ function preprocess_tiling(
         ref_img_cloudmasked = ref_image .* .!clouds_view
     end
 
-    @info "Step 3: Tiled adaptive histogram equalization"
-    clouds_red = to_uint8(float64.(red.(ref_img_cloudmasked) .* 255))
-    clouds_red[.!landmask.dilated] .= 0
-    equalized_rgbchannels = _process_image_tiles(
-        true_color_image, clouds_red, tiles, adapthisteq_params...
-    )
-    gammagreen = @view equalized_rgbchannels[:, :, 2]
-    equalized_gray = rgb2gray(equalized_rgbchannels)
+    begin
+        @debug "Step 3: Tiled adaptive histogram equalization"
+        clouds_red = to_uint8(float64.(red.(ref_img_cloudmasked) .* 255))
+        clouds_red[landmask.dilated] .= 0
 
-    @info "Step 4: Remove clouds from equalized_gray"
-    masks = [f.(ref_img_cloudmasked) .== 0 for f in [red, green, blue]]
-    combo_mask = reduce((a, b) -> a .& b, masks)
-    equalized_gray[combo_mask] .= 0
+        rgbchannels = _process_image_tiles(
+            true_color_image, clouds_red, tiles, adapthisteq_params...
+        )
 
-    @info "Step 5: unsharp_mask on equalized_gray and reconstruct"
-    sharpened = to_uint8(unsharp_mask(equalized_gray, unsharp_mask_params...))
-    equalized_gray_sharpened_reconstructed = reconstruct(
-        sharpened, structuring_elements.se_disk1, "dilation", true
-    )
-    equalized_gray_sharpened_reconstructed[.!landmask.dilated] .= 0
+        gammagreen = @view rgbchannels[:, :, 2]
+        equalized_gray = rgb2gray(rgbchannels)
+    end
+
+    begin
+        @debug "Step 4: Remove clouds from equalized_gray"
+        masks = [f.(ref_img_cloudmasked) .== 0 for f in [red, green, blue]]
+        combo_mask = reduce((a, b) -> a .& b, masks)
+        equalized_gray[combo_mask] .= 0
+    end
+
+    begin
+        @debug "Step 5: unsharp_mask on equalized_gray and reconstruct"
+        sharpened = to_uint8(unsharp_mask(equalized_gray, unsharp_mask_params...))
+        equalized_gray_sharpened_reconstructed = reconstruct(
+            sharpened, structuring_elements.se_disk1, "dilation", true
+        )
+        equalized_gray_sharpened_reconstructed[landmask.dilated] .= 0
+    end
 
     # TODO: Steps 6 and 7 can be done in parallel as they are independent
-    @info "# Step 6: Repeat step 5 with equalized_gray"
-    equalized_gray_reconstructed = deepcopy(equalized_gray)
-    equalized_gray_reconstructed[.!landmask.dilated] .= 0
-    equalized_gray_reconstructed = reconstruct(
-        equalized_gray_reconstructed, structuring_elements.se_disk1, "dilation", true
-    )
-    equalized_gray_reconstructed[.!landmask.dilated] .= 0
+    begin
+        @debug "Step 6: Repeat step 5 with equalized_gray (landmasking, no sharpening)"
+        equalized_gray_reconstructed = deepcopy(equalized_gray)
+        equalized_gray_reconstructed[landmask.dilated] .= 0
+        equalized_gray_reconstructed = reconstruct(
+            equalized_gray_reconstructed, structuring_elements.se_disk1, "dilation", true
+        )
+        equalized_gray_reconstructed[landmask.dilated] .= 0
+    end
 
-    @info "STEP 7: Brighten equalized_gray"
-    brighten = get_brighten_mask(equalized_gray_reconstructed, gammagreen)
-    equalized_gray[.!landmask.dilated] .= 0
-    equalized_gray .= imbrighten(equalized_gray, brighten, brighten_factor)
+    begin
+        @debug "STEP 7: Brighten equalized_gray"
+        brighten = get_brighten_mask(equalized_gray_reconstructed, gammagreen)
+        equalized_gray[landmask.dilated] .= 0
+        equalized_gray .= imbrighten(equalized_gray, brighten, brighten_factor)
+    end
 
-    @info "STEP 8: Get morphed_residue and adjust its gamma"
-    morphed_residue = clamp.(equalized_gray - equalized_gray_reconstructed, 0, 255)
-    agp = adjust_gamma_params
-    equalized_gray_sharpened_reconstructed_adjusted = imcomplement(
-        adjustgamma(equalized_gray_sharpened_reconstructed, agp.gamma)
-    )
-    adjusting_mask = equalized_gray_sharpened_reconstructed_adjusted .> agp.gamma_threshold
-    morphed_residue[adjusting_mask] .=
-        to_uint8.(morphed_residue[adjusting_mask] .* agp.gamma_factor)
+    begin
+        @debug "STEP 8: Get morphed_residue and adjust its gamma"
+        morphed_residue = clamp.(equalized_gray - equalized_gray_reconstructed, 0, 255)
 
-    @info "# Step 9: Get preliminary ice masks"
-    prelim_icemask, binarized_tiling = get_ice_masks(
-        ref_image, morphed_residue, landmask.dilated, tiles, true; ice_masks_params...
-    )
+        agp = adjust_gamma_params
+        equalized_gray_sharpened_reconstructed_adjusted = imcomplement(
+            adjustgamma(equalized_gray_sharpened_reconstructed, agp.gamma)
+        )
+        adjusting_mask =
+            equalized_gray_sharpened_reconstructed_adjusted .> agp.gamma_threshold
+        morphed_residue[adjusting_mask] .=
+            to_uint8.(morphed_residue[adjusting_mask] .* agp.gamma_factor)
+    end
 
-    @info "Step 10: Get segmentation mask from preliminary icemask"
-    segment_mask = get_segment_mask(prelim_icemask, binarized_tiling)
+    begin
+        @debug "Step 9: Get preliminary ice masks"
+        prelim_icemask, binarized_tiling = get_ice_masks(
+            ref_image, morphed_residue, landmask.dilated, tiles, true; ice_masks_params...
+        )
+    end
 
-    @info "Step 11: Get local_maxima_mask and L0mask via watershed"
-    local_maxima_mask, L0mask = watershed2(morphed_residue, segment_mask, prelim_icemask)
+    begin
+        @debug "Step 10: Get segmentation mask from preliminary icemask"
+        # Fill holes function in get_segment_mask a bit more aggressive than Matlabs
+        segment_mask = get_segment_mask(prelim_icemask, binarized_tiling)
+    end
 
-    @info "Step 12: Build icemask from all others"
-    local_maxima_mask = to_uint8(local_maxima_mask * 255)
-    prelim_icemask2 = _regularize(
-        morphed_residue,
-        local_maxima_mask,
-        segment_mask,
-        L0mask,
-        structuring_elements.se_disk1;
-        prelim_icemask_params...,
-    )
+    begin
+        @debug "Step 11: Get local_maxima_mask and L0mask via watershed"
+        local_maxima_mask, L0mask = watershed2(
+            morphed_residue, segment_mask, prelim_icemask
+        )
+    end
 
-    @info "Step 13: Get improved icemask"
-    icemask, _ = get_ice_masks(
-        ref_image, prelim_icemask2, landmask.dilated, tiles, false; ice_masks_params...
-    )
+    begin
+        @debug "Step 12: Build icemask from all others"
+        local_maxima_mask = to_uint8(local_maxima_mask * 255)
+        prelim_icemask2 = _regularize(
+            morphed_residue,
+            local_maxima_mask,
+            segment_mask,
+            L0mask,
+            structuring_elements.se_disk1;
+            prelim_icemask_params...,
+        )
+    end
 
-    @info "Step 14: Get final mask"
-    se = structuring_elements
-    se_erosion = se.se_disk1
-    se_dilation = se.se_disk2
-    final = get_final(icemask, segment_mask, se_erosion, se_dilation)
+    begin
+        @debug "Step 13: Get improved icemask"
+        icemask, _ = get_ice_masks(
+            ref_image, prelim_icemask2, landmask.dilated, tiles, false; ice_masks_params...
+        )
+    end
+
+    begin
+        @debug "Step 14: Get final mask"
+        se = structuring_elements
+        se_erosion = se.se_disk1
+        se_dilation = se.se_disk2
+        final = get_final(icemask, segment_mask, se_erosion, se_dilation)
+    end
 
     return final
 end
