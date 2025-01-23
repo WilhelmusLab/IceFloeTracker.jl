18af079dbb9aceb28346af6aae326e0d5292d008
Carlos Paniagua <cpaniaguam@gmail.com>
2025-01-23 15:08:27 -0500

    fix: update latlon for rasterio 1.4.3

--- src/latlon.py
+++ src/latlon.py
@@ -14,15 +14,15 @@ def getlatlon(imgpath):
     nrows, ncols = im.shape
     cols, rows = np.meshgrid(np.arange(ncols), np.arange(nrows))
     xs, ys = rasterio.transform.xy(im.transform, rows, cols)
-    xs = np.array(xs)
-    ys = np.array(ys)
-    
+    xs = xs.reshape(im.shape) # needed in rasterio 1.4.3
+    ys = ys.reshape(im.shape)
+
     # X and Y are the 1D index vectors
     X = xs[0, :]
     Y = ys[:, 0]
     ps_to_ll = Transformer.from_crs(im.crs, "WGS84", always_xy=True)
     lons, lats = ps_to_ll.transform(np.ravel(xs), np.ravel(ys))
-    
+
     # longitude and latitude are 2D index arrays
     longitude = np.reshape(lons, (nrows, ncols))
     latitude = np.reshape(lats, (nrows, ncols))
