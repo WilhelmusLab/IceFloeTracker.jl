3eebb8bc9425b93400d7662645fc2353d85fb06e
Carlos Paniagua <cpaniaguam@gmail.com>
2025-02-13 00:28:15 -0500

    feat: enhance long_tracker with condition and mc thresholds improve readability and structure

--- test/test-long-tracker.jl
+++ test/test-long-tracker.jl
@@ -1,4 +1,4 @@
-using IceFloeTracker: long_tracker, _imhist
+using IceFloeTracker: long_tracker, _imhist, condition_thresholds, mc_thresholds
 
 """
 addgaps(props)
@@ -11,33 +11,9 @@ function addgaps(props)
     # add gap after first day
     props = vcat(props[1:1], blank_props, props[2:end])
     # add gap before last day
-    props = vcat(props[1:end-1], blank_props, [props[end]])
+    props = vcat(props[1:(end - 1)], blank_props, [props[end]])
     return props
 end
-
-
-begin # Set thresholds
-    search_thresholds = (dt=(30.0, 100.0, 1300.0), dist=(200, 250, 300))
-    large_floe_settings = (
-        area=1200,
-        arearatio=0.28,
-        majaxisratio=0.10,
-        minaxisratio=0.12,
-        convexarearatio=0.14,
-    )
-    small_floe_settings = (
-        area=1200,
-        arearatio=0.18,
-        majaxisratio=0.1,
-        minaxisratio=0.15,
-        convexarearatio=0.2,
-    )
-    condition_thresholds = (search_thresholds, small_floe_settings, large_floe_settings)
-    mc_thresholds = (
-        goodness=(small_floe_area=0.18, large_floe_area=0.236, corr=0.68), comp=(mxrot=10, sz=16)
-    )
-end
-
 
 begin # Load data
     pth = joinpath("test_inputs", "tracker")
@@ -56,8 +32,8 @@ end
 begin # Filter out floes with area less than `floe_area_threshold` pixels
     floe_area_threshold = 400
     for (i, prop) in enumerate(_props)
-        _props[i] = prop[prop[:, :area].>=floe_area_threshold, :] # 500 working good
-        sort!(_props[i], :area, rev=true)
+        _props[i] = prop[prop[:, :area] .>= floe_area_threshold, :] # 500 working good
+        sort!(_props[i], :area; rev=true)
     end
 end
 
@@ -65,7 +41,9 @@ end
     @ntestset "Case 1" begin
         # Every floe is matched in every day
         props_test_case1 = deepcopy(_props)
-        trajectories = IceFloeTracker.long_tracker(props_test_case1, condition_thresholds, mc_thresholds)
+        trajectories = IceFloeTracker.long_tracker(
+            props_test_case1, condition_thresholds, mc_thresholds
+        )
 
         # Expected: 5 trajectories, all of which have length 3
         IDs = trajectories[!, :ID]
@@ -84,7 +62,9 @@ end
     end
 
     @ntestset "Case 2" begin
-        trajectories = IceFloeTracker.long_tracker(props_test_case2, condition_thresholds, mc_thresholds)
+        trajectories = IceFloeTracker.long_tracker(
+            props_test_case2, condition_thresholds, mc_thresholds
+        )
 
         # Expected: 5 trajectories, 3 of which have length 3 and 2 of which have length 2
         IDs = trajectories[!, :ID]
@@ -97,7 +77,9 @@ end
 
             props = addgaps(_props)
 
-            trajectories = IceFloeTracker.long_tracker(props, condition_thresholds, mc_thresholds)
+            trajectories = IceFloeTracker.long_tracker(
+                props, condition_thresholds, mc_thresholds
+            )
 
             # Expected: 5 trajectories, all of which have length 3 as in test case 1
             IDs = trajectories[!, :ID]
@@ -107,12 +89,13 @@ end
         @ntestset "Case 4" begin
             # Add gaps to props_test_case2
             props = addgaps(props_test_case2)
-            trajectories = IceFloeTracker.long_tracker(props, condition_thresholds, mc_thresholds)
+            trajectories = IceFloeTracker.long_tracker(
+                props, condition_thresholds, mc_thresholds
+            )
 
             # Expected: 5 trajectories, 3 of which have length 3 and 2 of which have length 2 as in test case 2
             IDs = trajectories[!, :ID]
             @test IDs == [1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5]
         end
     end
-
 end
