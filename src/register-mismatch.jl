4e12b7e8cae477def99e34804f5a526a3eae4099
Carlos Paniagua <cpaniaguam@gmail.com>
2025-03-05 21:26:20 -0500

    docs: clean up formatting in register-mismatch.jl documentation

--- src/register-mismatch.jl
+++ src/register-mismatch.jl
@@ -4,7 +4,7 @@
                   mxshift::Tuple{Int64, Int64}=(10,10),
                   mxrot::Float64=pi/4;
                   kwargs...
-                  )                   
+                  )
 
 Estimate a rigid transformation (translation + rotation) that minimizes the 'mismatch' of aligning `moving` with `fixed` using the [QuadDIRECT algorithm](https://github.com/timholy/QuadDIRECT.jl#readme).
 
@@ -16,8 +16,6 @@ Returns a pair with the mismatch score `mm` and the associated registration angl
 - `mxrot`: maximum allowed rotation in radians (default set to `π/4`)
 - `thresh`: minimum sum-of-squared-intensity overlap between the images (default is 10% of the sum-of-squared-intensity of `fixed`)
 - `kwargs`: other arguments such as `tol`, `ftol`, and `fvalue` (see [QuadDIRECT.analyze](https://github.com/timholy/QuadDIRECT.jl/blob/d6170f14a49f57552c59c9b4533a4b75a3ab3c45/src/algorithm.jl#L459) for details)
-
-```
 """
 function mismatch(
     fixed::AbstractArray,
