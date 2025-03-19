e7f19f00b087fbbb73d04993685298baab6de872
Carlos Paniagua <cpaniaguam@gmail.com>
2025-03-19 16:56:51 -0400

    test: latlon.jl

--- test/test-latlon.jl
+++ test/test-latlon.jl
@@ -1,27 +1,42 @@
 using IceFloeTracker: getlatlon
 
-@testset "getlatlon" begin
-    imgpth = "test_inputs/latlon/latlon_test_image-2020-06-21T00_00_00Z.tif"
+function round4(v)
+    # round to 4 decimal places to avoid weirdness in different arch/os
+    _round4(x) = round(x; digits=4)
+    return _round4.(v)
+end
 
+@testset "latlon" begin
+    imgpth = "test_inputs/latlon/latlon_test_image-2020-06-21T00_00_00Z.tif"
     # vec needed to convert to vector instead of nx1 matrix
     expected_X = vec(readdlm("test_inputs/latlon/X.csv", ',', Float64))
     expected_Y = vec(readdlm("test_inputs/latlon/Y.csv", ',', Float64))
 
     expected_lat = readdlm("test_inputs/latlon/latitude.csv", ',', Float64)
     expected_lon = readdlm("test_inputs/latlon/longitude.csv", ',', Float64)
+    expected_lat, expected_lon, expected_X, expected_Y =
+        round4.([expected_lat, expected_lon, expected_X, expected_Y])
 
-    latlon = getlatlon(imgpth)
-    X = latlon["X"]
-    Y = latlon["Y"]
-    lat = latlon["latitude"]
-    lon = latlon["longitude"]
+    @testset "getlatlon.py" begin
+        latlon = getlatlon(imgpth)
+        X = latlon["X"]
+        Y = latlon["Y"]
+        lat = latlon["latitude"]
+        lon = latlon["longitude"]
+        lat, lon, X, Y = round4.([lat, lon, X, Y])
 
-    # round to 4 decimal places to avoid weirdness in different arch/os
-    expected_lat, expected_lon, expected_X, expected_Y = [round.(arr, digits=4) for arr in [expected_lat, expected_lon, expected_X, expected_Y]]
-    lat, lon, X, Y = [round.(arr, digits=4) for arr in [lat, lon, X, Y]]
+        @test expected_X == X
+        @test expected_Y == Y
+        @test expected_lat == lat
+        @test expected_lon == lon
+    end
+    @testset "latlon.jl" begin
+        _, lon, lat, X, Y = latlon(img)
+        lat, lon, X, Y = round4.([lat, lon, X, Y])
 
-    @test expected_X == X
-    @test expected_Y == Y
-    @test expected_lat == lat
-    @test expected_lon == lon
+        @test expected_X == X
+        @test expected_Y == Y
+        @test expected_lat == lat
+        @test expected_lon == lon
+    end
 end
