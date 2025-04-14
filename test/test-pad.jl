82f7caeec19c319eed21f6787675c4b1db83e979
John Gerrard Holland <john_holland1@brown.edu>
2025-04-14 16:02:28 +0000

    remove extra show lines

--- test/test-pad.jl
+++ test/test-pad.jl
@@ -3,9 +3,7 @@ using IceFloeTracker: pad_images
 @testset "padding" begin
     @testset "two identical inputs are padded the same" begin
         function test_padding_for_identical_inputs(image, expected_result)
-            # display(image)
             result = pad_images(image, image)
-            # display(result[1])
             @test result[1] == result[2]  # results are the same
             @test result[1] == expected_result  # results are as expected
         end
@@ -135,9 +133,7 @@ using IceFloeTracker: pad_images
     @testset "unlike images are padded similarly" begin
         function test_padding_for_unlike_images(image1, image2, expected_result1, expected_result2)
             result1, result2 = pad_images(image1, image2)
-            show(result1)
             @test result1 == expected_result1
-            show(result2)
             @test result2 == expected_result2
         end
         @testset "minimum example" begin
