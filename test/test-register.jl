4eb3672cee0e930fa22efa58473a01045ea71545
John Gerrard Holland <john_holland1@brown.edu>
2025-04-15 23:14:27 +0000

    delete test-register-mismatch testfile

--- test/test-register.jl
+++ test/test-register.jl
@@ -2062,3 +2062,23 @@ end
         end
     end
 end
+
+@testset "registration mismatch tests" begin
+    # Read in floe from file
+    floe = readdlm("./test_inputs/floetorotate.csv", ',', Bool)
+
+    # Define rotation angle
+    rot_angle = 15 # degrees
+
+    # Rotate floe
+    rotated_floe = imrotate(floe, deg2rad(rot_angle))
+
+    # Estimate rigid transformation and mismatch
+    mm, rot = IceFloeTracker.mismatch(floe, rotated_floe)
+
+    # Test 1: mismatch accuracy
+    @test mm < 0.0055
+
+    # Test 2: angle estimate
+    @test abs(rot - rot_angle) < 0.5
+end
