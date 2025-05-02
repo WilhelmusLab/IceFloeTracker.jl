2042f834dd1834dd6a73ca74e7eccfcb42734008
John Gerrard Holland <john_holland1@brown.edu>
2025-05-02 19:57:46 +0000

    remove redundant comment

--- src/tracker/long_tracker.jl
+++ src/tracker/long_tracker.jl
@@ -73,7 +73,6 @@ function long_tracker(props::Vector{DataFrame}, condition_thresholds, mc_thresho
         @show trajectories
     end
     # trajectories = IceFloeTracker.drop_trajectories_length1(trajectories, :head_uuid)
-    # list the uuid in the leftmost column
     add_id!(trajectories, :head_uuid, :ID)
     cols = [col for col in names(trajectories) if col ∉ ["ID", "head_uuid", "uuid"]]
     return trajectories[!, ["ID", "head_uuid", "uuid", cols...]]
