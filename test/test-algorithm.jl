89250925be6f7b60f19d54169a5dcf031926ee55
John Gerrard Holland <john_holland1@brown.edu>
2025-07-03 19:41:34 +0000

    add image saving for segment image

--- test/test-algorithm.jl
+++ test/test-algorithm.jl
@@ -1,3 +1,5 @@
+using ImageSegmentation: segment_labels, segment_mean, labels_map
+
 @ntestset "$(@__FILE__)" begin
     @ntestset "Lopez-Acosta 2019" begin
         loadimg(s::String) = float64.(load(s))
@@ -13,6 +15,14 @@
         landmask = loadimg("./test_inputs/pipeline/input_pipeline/landmask.tiff")
 
         segments = LopezAcosta2019()(truecolor, falsecolor, landmask)
+
         @show segments
+        save(
+            "./test_outputs/Lopez-Acosta-2019-mean-labels" *
+            Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS") *
+            ".png",
+            map(i -> segment_mean(segments, i), labels_map(segments)),
+        )
+        @test length(segment_labels(segments)) == 44
     end
 end
