e25e1bb0818ee49f4c01357e9c8b5fb80cc91bcf
John Gerrard Holland <john_holland1@brown.edu>
2025-07-03 15:29:10 +0000

    update test formatting

--- test/test-algorithm.jl
+++ test/test-algorithm.jl
@@ -1,19 +1,18 @@
-@testset "algorithm tests" begin
-    println("-------------------------------------------------")
-    println("----------- high-level algorithm tests ----------")
+@ntestset "$(@__FILE__)" begin
+    @ntestset "Lopez-Acosta 2019" begin
+        loadimg(s::String) = float64.(load(s))
 
-    loadimg(s::String) = float64.(load(s))
+        truecolor = loadimg(
+            "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
+        )
 
-    truecolor = loadimg(
-        "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
-    )
+        falsecolor = loadimg(
+            "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
+        )
 
-    falsecolor = loadimg(
-        "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
-    )
+        landmask = loadimg("./test_inputs/pipeline/input_pipeline/landmask.tiff")
 
-    landmask = loadimg("./test_inputs/pipeline/input_pipeline/landmask.tiff")
-
-    results = LopezAcosta2019()(truecolor, falsecolor, landmask)
-    @info results
+        results = LopezAcosta2019()(truecolor, falsecolor, landmask)
+        @show results
+    end
 end
