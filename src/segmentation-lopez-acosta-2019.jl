cecd325f043a19d7fff7d9208a5a6c2864a7b14e
John Gerrard Holland <john_holland1@brown.edu>
2025-07-08 12:48:36 +0000

    remove references to IceFloeTracker in algorithm

--- src/segmentation-lopez-acosta-2019.jl
+++ src/segmentation-lopez-acosta-2019.jl
@@ -19,52 +19,49 @@ function (p::LopezAcosta2019)(
 
     # 2. Intermediate images
     @info "Finding ice labels"
-    ice_labels = IceFloeTracker.find_ice_labels(falsecolor_image, landmask_imgs.dilated)
+    ice_labels = find_ice_labels(falsecolor_image, landmask_imgs.dilated)
 
     @info "Sharpening truecolor image"
     # a. apply imsharpen to truecolor image using non-dilated landmask
-    sharpened_truecolor_image = IceFloeTracker.imsharpen(
-        truecolor_image, landmask_imgs.non_dilated
-    )
+    sharpened_truecolor_image = imsharpen(truecolor_image, landmask_imgs.non_dilated)
     # b. apply imsharpen to sharpened truecolor img using dilated landmask
-    sharpened_gray_truecolor_image = IceFloeTracker.imsharpen_gray(
+    sharpened_gray_truecolor_image = imsharpen_gray(
         sharpened_truecolor_image, landmask_imgs.dilated
     )
 
     @info "Normalizing truecolor image"
-    normalized_image = IceFloeTracker.normalize_image(
+    normalized_image = normalize_image(
         sharpened_truecolor_image, sharpened_gray_truecolor_image, landmask_imgs.dilated
     )
 
     # Discriminate ice/water
     @info "Discriminating ice/water"
-    ice_water_discrim = IceFloeTracker.discriminate_ice_water(
+    ice_water_discrim = discriminate_ice_water(
         falsecolor_image, normalized_image, copy(landmask_imgs.dilated), cloudmask
     )
 
     # 3. Segmentation
     @info "Segmenting floes part 1/3"
-    segA = IceFloeTracker.segmentation_A(
-        IceFloeTracker.segmented_ice_cloudmasking(ice_water_discrim, cloudmask, ice_labels)
+    segA = segmentation_A(
+        segmented_ice_cloudmasking(ice_water_discrim, cloudmask, ice_labels)
     )
 
     # segmentation_B
     @info "Segmenting floes part 2/3"
-    segB = IceFloeTracker.segmentation_B(sharpened_gray_truecolor_image, cloudmask, segA)
+    segB = segmentation_B(sharpened_gray_truecolor_image, cloudmask, segA)
 
     # Process watershed in parallel using Folds
     @info "Building watersheds"
     # container_for_watersheds = [landmask_imgs.non_dilated, similar(landmask_imgs.non_dilated)]
 
     watersheds_segB = [
-        IceFloeTracker.watershed_ice_floes(segB.not_ice_bit),
-        IceFloeTracker.watershed_ice_floes(segB.ice_intersect),
+        watershed_ice_floes(segB.not_ice_bit), watershed_ice_floes(segB.ice_intersect)
     ]
-    watersheds_segB_product = IceFloeTracker.watershed_product(watersheds_segB...)
+    watersheds_segB_product = watershed_product(watersheds_segB...)
 
     # segmentation_F
     @info "Segmenting floes part 3/3"
-    segF = IceFloeTracker.segmentation_F(
+    segF = segmentation_F(
         segB.not_ice,
         segB.ice_intersect,
         watersheds_segB_product,
