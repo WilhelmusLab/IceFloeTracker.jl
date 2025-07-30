74a099e55e2af21270fe53ae750e3da79461d67e
John Gerrard Holland <john_holland1@brown.edu>
2025-07-30 17:11:28 +0000

    only attempt to save valid images

--- test/test-segmentation-lopez-acosta-2019.jl
+++ test/test-segmentation-lopez-acosta-2019.jl
@@ -1,6 +1,21 @@
 using Images: segment_labels, segment_mean, labels_map
 
 @ntestset "$(@__FILE__)" begin
+
+    # Symbols returned in `_, intermediate_results = LopezAcosta2019()(...; return_intermediate_results=true)`
+    # which can be written to PNGs using `save()`
+    intermediate_result_image_names = [
+        :landmask_dilated,
+        :landmask_non_dilated,
+        :cloudmask,
+        :sharpened_truecolor_image,
+        :sharpened_gray_truecolor_image,
+        :normalized_image,
+        :segA,
+        :watersheds_segB_product,
+        :segF,
+    ]
+
     @ntestset "Lopez-Acosta 2019" begin
         truecolor = load(
             "./test_inputs/pipeline/input_pipeline/20220914.aqua.truecolor.250m.tiff"
@@ -9,7 +24,7 @@ using Images: segment_labels, segment_mean, labels_map
             "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
         )
         landmask = load("./test_inputs/pipeline/input_pipeline/landmask.tiff")
-        @ntestset "Smoke test" begin
+        @ntestset "Image types" begin
             region = (200:400, 500:700)
             supported_types = [n0f8, n6f10, n4f12, n2f14, n0f16, float32, float64]
             for target_type in supported_types
@@ -23,7 +38,8 @@ using Images: segment_labels, segment_mean, labels_map
                 datestamp = Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")
                 save_intermediate_images(
                     "./test_outputs/segmentation-Lopez-Acosta-2019-$(target_type)-$(datestamp)-intermediate-results/",
-                    intermediate_results,
+                    intermediate_results;
+                    names=intermediate_result_image_names,
                 )
                 @show segments
                 save(
