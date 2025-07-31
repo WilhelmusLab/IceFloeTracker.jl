4a3d8b0fc2d42395505cd5e7a3b539475db0e84b
John Gerrard Holland <john_holland1@brown.edu>
2025-07-31 15:58:09 +0000

    add data validation object return

--- test/download.jl
+++ test/download.jl
@@ -1,17 +1,37 @@
 using Downloads: download, RequestError
 using CSV
 using Dates
+using Images: SegmentedImage, Colorant, Gray
+using FileIO: load
+using CSVFiles
+using DataFrames
 
-abstract type ValidationData end
+@kwdef struct ValidationDataCase
+    metadata::Union{AbstractDict,Missing} = missing
 
-@kwdef struct Watkins2025GitHub <: ValidationData
+    # TODO: add types
+    modis_truecolor::Union{AbstractArray,Missing} = missing
+    modis_falsecolor::Union{AbstractArray,Missing} = missing
+    modis_landmask::Union{AbstractArray,Missing} = missing
+    modis_cloudfraction::Union{AbstractArray,Missing} = missing
+    maisie_landmask::Union{AbstractArray,Missing} = missing
+    maisie_seaice::Union{AbstractArray,Missing} = missing
+
+    validated_binary_floes::Union{AbstractArray{Gray{Bool}},Missing} = missing
+    validated_labeled_floes::Union{SegmentedImage,Missing} = missing
+    validated_floe_properties::Union{CSVFiles.CSVFile,Missing} = missing
+end
+
+abstract type ValidationDataLoader end
+
+@kwdef struct Watkins2025GitHub <: ValidationDataLoader
     url::AbstractString = "https://raw.githubusercontent.com/danielmwatkins/ice_floe_validation_dataset/refs/heads/"
     branch::AbstractString = "main"
     dataset_metadata_path::AbstractString = "data/validation_dataset/validation_dataset.csv"
     target_directory::AbstractString = "./Watkins2025GitHub"
 end
 
-function (p::ValidationData)(; kwargs...)
+function (p::ValidationDataLoader)(; kwargs...)
     metadata_url = joinpath(p.url, p.branch, p.dataset_metadata_path)
     metadata_path = joinpath(
         p.target_directory, p.branch, splitpath(p.dataset_metadata_path)[end]
@@ -25,6 +45,11 @@ function (p::ValidationData)(; kwargs...)
 end
 
 function load_case(case::CSV.Row, p::Watkins2025GitHub)
+    validation_data_dict = Dict()
+    validation_data_dict[:metadata] = Dict(
+        symbol => case[symbol] for symbol in propertynames(case)
+    )
+
     case_number = lpad(case.case_number, 3, "0")
     region = case.region
     date = Dates.format(case.start_date, "yyyymmdd")
@@ -46,41 +71,50 @@ function load_case(case::CSV.Row, p::Watkins2025GitHub)
     modis_truecolor = (;
         source="data/modis/truecolor/$(case_number)-$(region)-$(image_side_length)-$(date).$(satellite).truecolor.$(pixel_scale).$(ext)",
         target="modis_truecolor.$(ext)",
+        name=:modis_truecolor,
     )
     modis_falsecolor = (;
         source="data/modis/falsecolor/$(case_number)-$(region)-$(image_side_length)-$(date).$(satellite).falsecolor.$(pixel_scale).$(ext)",
         target="modis_falsecolor.$(ext)",
+        name=:modis_falsecolor,
     )
     modis_landmask = (;
         source="data/modis/landmask/$(case_number)-$(region)-$(image_side_length)-$(date).$(satellite).landmask.$(pixel_scale).$(ext)",
         target="modis_landmask.$(ext)",
+        name=:modis_landmask,
     )
     modis_cloudfraction = (;
         source="data/modis/cloudfraction/$(case_number)-$(region)-$(image_side_length)-$(date).$(satellite).cloudfraction.$(pixel_scale).$(ext)",
         target="modis_cloudfraction.$(ext)",
+        name=:modis_cloudfraction,
     )
     maisie_landmask = (;
         source="data/masie/landmask/$(case_number)-$(region)-$(image_side_length)-$(date).masie.landmask.$(pixel_scale).$(ext)",
         target="maisie_landmask.$(ext)",
+        name=:maisie_landmask,
     )
     maisie_seaice = (;
         source="data/masie/seaice/$(case_number)-$(region)-$(image_side_length)-$(date).masie.seaice.$(pixel_scale).$(ext)",
         target="maisie_seaice.$(ext)",
+        name=:maisie_seaice,
     )
     validated_binary_floes = (;
         source="data/validation_dataset/binary_floes/$(case_number)-$(region)-$(date)-$(satellite)-binary_floes.png",
         target="validated_binary_floes.png",
+        name=:validated_binary_floes,
     )
     validated_labeled_floes = (;
         source="data/validation_dataset/labeled_floes/$(case_number)-$(region)-$(date)-$(satellite)-labeled_floes.$(ext)",
         target="validated_labeled_floes.$(ext)",
+        name=:validated_labeled_floes,
     )
     validated_floe_properties = (;
         source="data/validation_dataset/property_tables/$(satellite)/$(case_number)-$(region)-$(date)-$(satellite)-floe_properties.csv",
         target="validated_floe_properties.csv",
+        name=:validated_floe_properties,
     )
 
-    for image in [
+    for file_information in [
         modis_truecolor,
         modis_falsecolor,
         modis_landmask,
@@ -91,20 +125,42 @@ function load_case(case::CSV.Row, p::Watkins2025GitHub)
         validated_labeled_floes,
         validated_floe_properties,
     ]
-        image_url = joinpath(p.url, p.branch, image.source)
-        image_path = joinpath(output_directory, image.target)
-        isfile(image_path) && continue  # don't download a second time if we already have the file
-        @debug "downloading $(image_url) to $(image_path)"
         try
-            download(image_url, image_path)
+            file_url = joinpath(p.url, p.branch, file_information.source)
+            file_path = joinpath(output_directory, file_information.target)
+            validation_data_dict[file_information.name] = get_file(file_url, file_path)
         catch e
             if isa(e, RequestError)
-                @show "$(image_url) missing"
+                @show "$(file_url) missing"
             else
                 rethrow(e)
             end
         end
     end
 
-    return nothing
+    # Conversions
+    validation_data_dict[:validated_labeled_floes] = SegmentedImage(
+        validation_data_dict[:modis_truecolor],
+        Int.(validation_data_dict[:validated_labeled_floes]),
+    )
+    validation_data_dict[:validated_binary_floes] =
+        Gray.(Gray.(validation_data_dict[:validated_binary_floes]) .> 0.5)
+
+    validation_data = ValidationDataCase(; validation_data_dict...)
+    return validation_data
+end
+
+function get_file(file_url, file_path)
+    try
+        isfile(file_path) || download(file_url, file_path)
+        file = load(file_path)
+        return file
+    catch e
+        if isa(e, RequestError)
+            @show "$(file_url) missing"
+            return nothing
+        else
+            rethrow(e)
+        end
+    end
 end
