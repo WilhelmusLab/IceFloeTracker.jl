753c03e44ae95e0eff6896306284ea132bd8d672
John Gerrard Holland <john_holland1@brown.edu>
2025-07-31 21:08:07 +0000

    add beginnings of segmentation validation

--- test/test-segmentation-lopez-acosta-2019.jl
+++ test/test-segmentation-lopez-acosta-2019.jl
@@ -2,31 +2,6 @@ using Images: segment_labels, segment_mean, labels_map
 
 @ntestset "$(@__FILE__)" begin
     @ntestset "Lopez-Acosta 2019" begin
-        truecolor = load(
-            "./test_inputs/pipeline/input_pipeline/20220914.aqua.truecolor.250m.tiff"
-        )
-        falsecolor = load(
-            "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
-        )
-        landmask = load("./test_inputs/pipeline/input_pipeline/landmask.tiff")
-        @ntestset "Smoke test" begin
-            region = (200:400, 500:700)
-            supported_types = [n0f8, n6f10, n4f12, n2f14, n0f16, float32, float64]
-            for target_type in supported_types
-                @info "Image type: $target_type"
-                segments = LopezAcosta2019()(
-                    target_type.(truecolor[region...]),
-                    target_type.(falsecolor[region...]),
-                    target_type.(landmask[region...]),
-                )
-                @show segments
-                save(
-                    "./test_outputs/segmentation-Lopez-Acosta-2019-mean-labels_$(target_type)_$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")).png",
-                    map(i -> segment_mean(segments, i), labels_map(segments)),
-                )
-                @test length(segment_labels(segments)) == 10
-            end
-        end
         @ntestset "Validated data" begin
             data_loader = Watkins2025GitHub(;
                 cache_dir="./__temp__/Watkins2025GitHub",
@@ -47,7 +22,18 @@ using Images: segment_labels, segment_mean, labels_map
                         "./test_outputs/segmentation-LopezAcosta2019-$(name)_$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS"))-mean-labels.png",
                         map(i -> segment_mean(segments, i), labels_map(segments)),
                     )
-                    push!(results, (; name, success=true, error=nothing))
+                    if !isnothing(validation_data.validated_labeled_floes)
+                        area = labels_map(segments) .> 0
+                        validated_area =
+                            labels_map(validation_data.validated_labeled_floes) .> 0
+                    else
+                        area = nothing
+                        validated_area = nothing
+                    end
+
+                    push!(
+                        results, (; name, success=true, area, validated_area, error=nothing)
+                    )
                 catch e
                     @warn "$(name) failed, $(e)"
                     push!(results, (; name, success=false, error=e))
@@ -56,14 +42,41 @@ using Images: segment_labels, segment_mean, labels_map
                 @info sort(results_df)
             end
         end
-        @ntestset "Full size" begin
-            segments = LopezAcosta2019()(truecolor, falsecolor, landmask)
-            @show segments
-            save(
-                "./test_outputs/segmentation-Lopez-Acosta-2019-mean-labels_$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")).png",
-                map(i -> segment_mean(segments, i), labels_map(segments)),
-            )
-            @test length(segment_labels(segments)) == 44
-        end
+
+        # truecolor = load(
+        #     "./test_inputs/pipeline/input_pipeline/20220914.aqua.truecolor.250m.tiff"
+        # )
+        # falsecolor = load(
+        #     "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
+        # )
+        # landmask = load("./test_inputs/pipeline/input_pipeline/landmask.tiff")
+        # @ntestset "Smoke test" begin
+        #     region = (200:400, 500:700)
+        #     supported_types = [n0f8, n6f10, n4f12, n2f14, n0f16, float32, float64]
+        #     for target_type in supported_types
+        #         @info "Image type: $target_type"
+        #         segments = LopezAcosta2019()(
+        #             target_type.(truecolor[region...]),
+        #             target_type.(falsecolor[region...]),
+        #             target_type.(landmask[region...]),
+        #         )
+        #         @show segments
+        #         save(
+        #             "./test_outputs/segmentation-Lopez-Acosta-2019-mean-labels_$(target_type)_$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")).png",
+        #             map(i -> segment_mean(segments, i), labels_map(segments)),
+        #         )
+        #         @test length(segment_labels(segments)) == 10
+        #     end
+        # end
+
+        # @ntestset "Full size" begin
+        #     segments = LopezAcosta2019()(truecolor, falsecolor, landmask)
+        #     @show segments
+        #     save(
+        #         "./test_outputs/segmentation-Lopez-Acosta-2019-mean-labels_$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")).png",
+        #         map(i -> segment_mean(segments, i), labels_map(segments)),
+        #     )
+        #     @test length(segment_labels(segments)) == 44
+        # end
     end
 end
