a60f0ef4ae75cba871bfc64b932c9280fcac2aef
John Gerrard Holland <john_holland1@brown.edu>
2025-08-01 14:53:38 +0000

    add documentation for `segmentation_comparison`

--- test/test-segmentation-lopez-acosta-2019.jl
+++ test/test-segmentation-lopez-acosta-2019.jl
@@ -1,5 +1,14 @@
 using Images: segment_labels, segment_mean, labels_map
 
+"""
+Compares two SegmentedImages and returns values describing how similar the segmentations are.
+
+Treats the segment labeled `0` as background.
+
+Measures:
+- normalized_{validated,measured}_area: fraction of image covered by segments
+- fractional_intersection: fraction of the validated segments covered by measured segments
+"""
 function segmentation_comparison(;
     validated::Union{SegmentedImage,Nothing}=nothing,
     measured::Union{SegmentedImage,Nothing}=nothing,
@@ -7,6 +16,7 @@ function segmentation_comparison(;
     if !isnothing(validated)
         validated_binary = Gray.(labels_map(validated) .> 0)
         validated_area = sum(channelview(validated_binary))
+        normalized_validated_area = validated_area / length(channelview(validated_binary))
     else
         validated_area = missing
     end
@@ -14,6 +24,7 @@ function segmentation_comparison(;
     if !isnothing(measured)
         measured_binary = Gray.(labels_map(measured) .> 0)
         measured_area = sum(channelview(measured_binary))
+        normalized_measured_area = measured_area / length(channelview(measured_binary))
     else
         measured_area = missing
     end
@@ -26,7 +37,7 @@ function segmentation_comparison(;
         fractional_intersection = missing
     end
 
-    return (; measured_area, validated_area, fractional_intersection)
+    return (; normalized_validated_area, normalized_measured_area, fractional_intersection)
 end
 
 function run_segmentation_over_multiple_cases(
