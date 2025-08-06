0efd283aa631990cbac7912a58b331972c7dc2f8
John Gerrard Holland <john_holland1@brown.edu>
2025-08-06 20:02:57 +0000

    update docs for segmentation_comparison

--- src/segmented-image-utilities.jl
+++ src/segmented-image-utilities.jl
@@ -1,19 +1,32 @@
 """
+Results of a segmentation comparison
+"""
+SegmentationComparison = @NamedTuple begin
+    recall::Union{Real,Missing}
+    precision::Union{Real,Missing}
+    F_score::Union{Real,Missing}
+end
+
+"""
 function segmentation_comparison(
     validated::SegmentedImage, measured::SegmentedImage
 )::@NamedTuple{recall::Real, precision::Real, F_score::Real}
 
 Compares two SegmentedImages and returns values describing how similar the segmentations are.
 
-Treats the segment labeled `0` as background.
+This treats the segment labeled `0` as background.
 
 Measures:
-- normalized_{validated,measured}_area: fraction of image covered by segments
-- fractional_intersection: fraction of the validated segments covered by measured segments
+- precision: rate at which pixels in `validated` segments belong to `measured` segments
+- recall: rate at which pixels in `measured` segments belong to `validated` segments
+- F_score: harmonic mean of precision and recall
 """
 function segmentation_comparison(
-    validated::Union{SegmentedImage}, measured::Union{SegmentedImage}
-)::@NamedTuple{recall::Real, precision::Real, F_score::Real}
+    validated::Union{SegmentedImage,Nothing}, measured::Union{SegmentedImage,Nothing}
+)::SegmentationComparison
+    (isnothing(validated) || isnothing(measured)) &&
+        return (; recall=missing, precision=missing, F_score=missing)
+
     validated_binary = binarize(validated)
     measured_binary = binarize(measured)
     intersection = measured_binary .&& validated_binary
@@ -23,14 +36,14 @@ function segmentation_comparison(
     return (; recall, precision, F_score)
 end
 
-function binarize(segments::SegmentedImage)
-    return labels_map(segments) .> 0
+function segmentation_comparison(;
+    validated::Union{SegmentedImage,Nothing}, measured::Union{SegmentedImage,Nothing}
+)::SegmentationComparison
+    return segmentation_comparison(validated, measured)
 end
 
-function segmentation_comparison(;
-    validated::Union{SegmentedImage}, measured::Union{SegmentedImage}
-)::NamedTuple
-    return segmentation_comparison(validated, measured)
+function binarize(segments::SegmentedImage)
+    return labels_map(segments) .> 0
 end
 
 function segmentation_summary(
