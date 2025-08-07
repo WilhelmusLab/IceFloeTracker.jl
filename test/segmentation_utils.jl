e1907fe46649e875782cfa458e250088e0ed8e2b
John Gerrard Holland <john_holland1@brown.edu>
2025-08-07 21:00:19 +0000

    simplify run_segmentation_over_multiple_cases using the callback for results saving

--- test/segmentation_utils.jl
+++ test/segmentation_utils.jl
@@ -7,18 +7,35 @@ function run_segmentation_over_multiple_cases(
     cases,
     algorithm::IceFloeSegmentationAlgorithm;
     output_directory::Union{AbstractString,Nothing}=nothing,
+    result_images_to_save::Union{AbstractArray{Symbol},Nothing}=nothing,
 )::DataFrame
     results = []
     for case::ValidationDataCase in cases
+        datestamp = Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")
+
         let name, validated, measured, success, error, comparison
             name = case.name
             validated = case.validated_labeled_floes
+
+            if !isnothing(output_directory)
+                intermediate_results_callback = save_results_callback(
+                    joinpath(
+                        output_directory,
+                        "segmentation-$(typeof(algorithm))-$(name)-$(datestamp)",
+                    );
+                    names=result_images_to_save,
+                )
+            else
+                intermediate_results_callback = nothing
+            end
+
             @info "starting $(name)"
             try
                 measured = algorithm(
                     RGB.(case.modis_truecolor),
                     RGB.(case.modis_falsecolor),
-                    case.modis_landmask,
+                    case.modis_landmask;
+                    intermediate_results_callback,
                 )
                 @info "$(name) succeeded"
                 success = true
@@ -30,33 +47,15 @@ function run_segmentation_over_multiple_cases(
             end
 
             comparison = segmentation_comparison(; validated, measured)
-
             # Store the aggregate results
             push!(results, merge((; name, success, error), comparison))
 
-            if !isnothing(output_directory)
-                mkpath(output_directory)
-                datestamp = Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")
-                !isnothing(measured) && save(
-                    joinpath(
-                        output_directory,
-                        "segmentation-$(typeof(algorithm))-$(name)-$(datestamp)-mean-labels.png",
+            # Save the validated results we have them
+            if !ismissing(validated) && !isnothing(intermediate_results_callback)
+                intermediate_results_callback(;
+                    validated_mean_labels=map(
+                        i -> segment_mean(validated, i), labels_map(validated)
                     ),
-                    map(i -> segment_mean(measured, i), labels_map(measured)),
-                )
-                !isnothing(validated) && save(
-                    joinpath(
-                        output_directory,
-                        "segmentation-$(typeof(algorithm))-$(name)-$(datestamp)-validated-mean-labels.png",
-                    ),
-                    map(i -> segment_mean(validated, i), labels_map(validated)),
-                )
-                !isnothing(case.modis_truecolor) && save(
-                    joinpath(
-                        output_directory,
-                        "segmentation-$(typeof(algorithm))-$(name)-$(datestamp)-truecolor.png",
-                    ),
-                    case.modis_truecolor,
                 )
             end
         end
