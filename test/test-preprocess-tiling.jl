4dd1989079b611c65ffd56e15ba60995d80fcf2b
John Gerrard Holland <john_holland1@brown.edu>
2025-08-07 15:11:19 +0000

    remove extra indent

--- test/test-preprocess-tiling.jl
+++ test/test-preprocess-tiling.jl
@@ -14,43 +14,41 @@ using IceFloeTracker:
 include("segmentation_utils.jl")
 
 @testset "preprocess_tiling" begin
-    @ntestset "Full size" begin
-        region = (1016:3045, 1486:3714)
-        data_dir = joinpath(@__DIR__, "test_inputs")
-        true_color_image = load(
-            joinpath(data_dir, "beaufort-chukchi-seas_truecolor.2020162.aqua.250m.tiff")
-        )
-        ref_image = load(
-            joinpath(data_dir, "beaufort-chukchi-seas_falsecolor.2020162.aqua.250m.tiff")
-        )
-        landmask = float64.(load(joinpath(data_dir, "matlab_landmask.png"))) .> 0
+    region = (1016:3045, 1486:3714)
+    data_dir = joinpath(@__DIR__, "test_inputs")
+    true_color_image = load(
+        joinpath(data_dir, "beaufort-chukchi-seas_truecolor.2020162.aqua.250m.tiff")
+    )
+    ref_image = load(
+        joinpath(data_dir, "beaufort-chukchi-seas_falsecolor.2020162.aqua.250m.tiff")
+    )
+    landmask = float64.(load(joinpath(data_dir, "matlab_landmask.png"))) .> 0
 
-        # Crop images to region of interest
-        true_color_image, ref_image, landmask = [
-            img[region...] for img in (true_color_image, ref_image, landmask)
-        ]
+    # Crop images to region of interest
+    true_color_image, ref_image, landmask = [
+        img[region...] for img in (true_color_image, ref_image, landmask)
+    ]
 
-        landmask = (dilated=landmask,)
-        tiles = get_tiles(true_color_image; rblocks=2, cblocks=3)
+    landmask = (dilated=landmask,)
+    tiles = get_tiles(true_color_image; rblocks=2, cblocks=3)
 
-        foo = preprocess_tiling(
-            ref_image,
-            true_color_image,
-            landmask,
-            tiles,
-            cloud_mask_thresholds,
-            adapthisteq_params,
-            adjust_gamma_params,
-            structuring_elements,
-            unsharp_mask_params,
-            ice_masks_params,
-            prelim_icemask_params,
-            brighten_factor,
-        )
+    foo = preprocess_tiling(
+        ref_image,
+        true_color_image,
+        landmask,
+        tiles,
+        cloud_mask_thresholds,
+        adapthisteq_params,
+        adjust_gamma_params,
+        structuring_elements,
+        unsharp_mask_params,
+        ice_masks_params,
+        prelim_icemask_params,
+        brighten_factor,
+    )
 
-        # dmw: replace with test of mismatch against a preprocessed image
-        @test abs(sum(foo) - 1461116) / 1461116 < 0.1
-    end
+    # dmw: replace with test of mismatch against a preprocessed image
+    @test abs(sum(foo) - 1461116) / 1461116 < 0.1
 
     @ntestset "Validated data" begin
         data_loader = Watkins2025GitHub(; ref="a451cd5e62a10309a9640fbbe6b32a236fcebc70")
