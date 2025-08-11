0c9ff3d59559a23e5ccaec36c1f7ac14640075a4
John Gerrard Holland <john_holland1@brown.edu>
2025-08-11 20:10:43 +0000

    test[LopezAcosta2019Tiling]: functor and function implementations give similar results

--- test/test-preprocess-tiling.jl
+++ test/test-preprocess-tiling.jl
@@ -14,41 +14,93 @@ using IceFloeTracker:
 include("segmentation_utils.jl")
 
 @testset "preprocess_tiling" begin
-    region = (1016:3045, 1486:3714)
-    data_dir = joinpath(@__DIR__, "test_inputs")
-    true_color_image = load(
-        joinpath(data_dir, "beaufort-chukchi-seas_truecolor.2020162.aqua.250m.tiff")
-    )
-    ref_image = load(
-        joinpath(data_dir, "beaufort-chukchi-seas_falsecolor.2020162.aqua.250m.tiff")
-    )
-    landmask = float64.(load(joinpath(data_dir, "matlab_landmask.png"))) .> 0
-
-    # Crop images to region of interest
-    true_color_image, ref_image, landmask = [
-        img[region...] for img in (true_color_image, ref_image, landmask)
+    result_images_to_save = [
+        :ref_image,
+        :true_color_image,
+        :ref_img_cloudmasked,
+        :prelim_icemask,
+        :binarized_tiling,
+        :segment_mask,
+        :L0mask,
+        :icemask,
+        :final,
+        :segment_mean_truecolor,
+        :segment_mean_falsecolor,
     ]
+    @ntestset "established example" begin
+        region = (1016:3045, 1486:3714)
+        data_dir = joinpath(@__DIR__, "test_inputs")
+        true_color_image = load(
+            joinpath(data_dir, "beaufort-chukchi-seas_truecolor.2020162.aqua.250m.tiff")
+        )
+        ref_image = load(
+            joinpath(data_dir, "beaufort-chukchi-seas_falsecolor.2020162.aqua.250m.tiff")
+        )
+        landmask_image = load(joinpath(data_dir, "matlab_landmask.png"))
+        datestamp = Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS")
 
-    landmask = (dilated=landmask,)
-    tiles = get_tiles(true_color_image; rblocks=2, cblocks=3)
+        # Crop images to region of interest
+        true_color_image, ref_image, landmask_image = [
+            img[region...] for img in (true_color_image, ref_image, landmask_image)
+        ]
 
-    foo = preprocess_tiling(
-        ref_image,
-        true_color_image,
-        landmask,
-        tiles,
-        cloud_mask_thresholds,
-        adapthisteq_params,
-        adjust_gamma_params,
-        structuring_elements,
-        unsharp_mask_params,
-        ice_masks_params,
-        prelim_icemask_params,
-        brighten_factor,
-    )
+        tiles = get_tiles(true_color_image; rblocks=2, cblocks=3)
+        @ntestset "function version" begin
+            landmask = float64.(landmask_image) .> 0
+            binary_floe_mask = preprocess_tiling(
+                ref_image,
+                true_color_image,
+                (dilated=landmask,),
+                tiles,
+                cloud_mask_thresholds,
+                adapthisteq_params,
+                adjust_gamma_params,
+                structuring_elements,
+                unsharp_mask_params,
+                ice_masks_params,
+                prelim_icemask_params,
+                brighten_factor;
+                intermediate_results_callback=save_results_callback(
+                    "./test_outputs/segmentation-LopezAcosta2019Tiling-function-$(datestamp)";
+                    names=result_images_to_save,
+                ),
+            )
 
-    # dmw: replace with test of mismatch against a preprocessed image
-    @test abs(sum(foo) - 1461116) / 1461116 < 0.1
+            # dmw: replace with test of mismatch against a preprocessed image
+            @test abs(sum(binary_floe_mask) - 1461116) / 1461116 < 0.1
+
+            labels = label_components(binary_floe_mask)
+            segments = SegmentedImage(true_color_image, labels)
+            (; labeled_fraction) = segmentation_summary(segments)
+            @test labeled_fraction ≈ 0.3015 atol = 0.03
+        end
+        @ntestset "functor version" begin
+            segments = LopezAcosta2019Tiling(;
+                tile_settings=(; rblocks=2, cblocks=3),
+                cloud_mask_thresholds,
+                adapthisteq_params,
+                adjust_gamma_params,
+                structuring_elements,
+                unsharp_mask_params,
+                ice_masks_params,
+                prelim_icemask_params,
+                brighten_factor,
+            )(
+                true_color_image,
+                ref_image,
+                landmask_image;
+                intermediate_results_callback=save_results_callback(
+                    "./test_outputs/segmentation-LopezAcosta2019Tiling-functor-$(datestamp)";
+                    names=result_images_to_save,
+                ),
+            )
+            binary_floe_mask = labels_map(segments) .> 0
+            @test abs(sum(binary_floe_mask) - 1461116) / 1461116 < 0.1
+
+            (; labeled_fraction) = segmentation_summary(segments)
+            @test labeled_fraction ≈ 0.3015 atol = 0.03
+        end
+    end
 
     @ntestset "Validated data" begin
         data_loader = Watkins2025GitHub(; ref="a451cd5e62a10309a9640fbbe6b32a236fcebc70")
@@ -57,19 +109,7 @@ include("segmentation_utils.jl")
             case -> (case.case_number % 17 == 0),
             LopezAcosta2019Tiling();
             output_directory="./test_outputs/",
-            result_images_to_save=[
-                :ref_image,
-                :true_color_image,
-                :ref_img_cloudmasked,
-                :prelim_icemask,
-                :binarized_tiling,
-                :segment_mask,
-                :L0mask,
-                :icemask,
-                :final,
-                :segment_mean_truecolor,
-                :segment_mean_falsecolor,
-            ],
+            result_images_to_save,
         )
         @test all(results.success)
     end
