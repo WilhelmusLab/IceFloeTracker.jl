0c1660b9a45566f1039fdb0289c1e68751dda61b
John Gerrard Holland <john_holland1@brown.edu>
2025-08-12 14:11:07 +0000

    remove old detailed test case with local large data

--- test/test-preprocess-tiling.jl
+++ test/test-preprocess-tiling.jl
@@ -52,41 +52,7 @@ include("segmentation_utils.jl")
             @test 0.457 ≤ F_score
         end
     end
-
-    # Crop images to region of interest
-    true_color_image, ref_image, landmask_image = [
-        img[region...] for img in (true_color_image, ref_image, landmask_image)
-    ]
-
-    tile_settings = (; rblocks=2, cblocks=3)
-
-    segments = LopezAcosta2019Tiling(;
-        tile_settings,
-        cloud_mask_thresholds,
-        adapthisteq_params,
-        adjust_gamma_params,
-        structuring_elements,
-        unsharp_mask_params,
-        ice_masks_params,
-        prelim_icemask_params,
-        brighten_factor,
-    )(
-        true_color_image,
-        ref_image,
-        landmask_image;
-        intermediate_results_callback=save_results_callback(
-            "./test_outputs/segmentation-LopezAcosta2019Tiling-functor-$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS"))";
-        ),
-    )
-    binary_floe_mask = binarize_segments(segments)
-    @test abs(sum(binary_floe_mask) - 1461116) / 1461116 < 0.1
-
-    # Same test, a different way
-    (; labeled_fraction) = segmentation_summary(segments)
-    @test labeled_fraction ≈ 0.3015 atol = 0.03
-
-    @ntestset "Validated data" begin
-        data_loader = Watkins2025GitHub(; ref="a451cd5e62a10309a9640fbbe6b32a236fcebc70")
+    @ntestset "Aggregate results" begin
         results = run_segmentation_over_multiple_cases(
             data_loader,
             case -> (case.case_number % 17 == 0),
@@ -116,8 +82,8 @@ include("segmentation_utils.jl")
         @test mean_F_score ≥ 0.1
 
         # return current performance
-        @info mean_recall
-        @info mean_precision
-        @info mean_F_score
+        @show mean_recall
+        @show mean_precision
+        @show mean_F_score
     end
 end
