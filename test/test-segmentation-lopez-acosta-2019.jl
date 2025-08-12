70651a544329b2bddb73ce900edb8051cd09a182
John Gerrard Holland <john_holland1@brown.edu>
2025-08-12 17:16:09 +0000

    test[LopezAcosta2019Tiling]: add validated testcases

--- test/test-segmentation-lopez-acosta-2019.jl
+++ test/test-segmentation-lopez-acosta-2019.jl
@@ -19,15 +19,50 @@ using Images: segment_labels, segment_mean, labels_map
             @test all(filter(!broken_cases, results).success)
             @test any(filter(broken_cases, results).success) broken = true
         end
-        truecolor = load(
-            "./test_inputs/pipeline/input_pipeline/20220914.aqua.truecolor.250m.tiff"
-        )
-        falsecolor = load(
-            "./test_inputs/pipeline/input_pipeline/20220914.aqua.reflectance.250m.tiff"
-        )
-        landmask = load("./test_inputs/pipeline/input_pipeline/landmask.tiff")
+        @ntestset "Detailed tests" begin
+            check_case(
+                LopezAcosta2019(),
+                data_loader,
+                c -> (c.case_number == 6 && c.satellite == "terra");
+                labeled_fraction_goal=0.119,
+                recall_goal=0.315,
+                precision_goal=0.770,
+                F_score_goal=0.447,
+            )
+            check_case(
+                LopezAcosta2019(),
+                data_loader,
+                c -> (c.case_number == 14 && c.satellite == "aqua");
+                labeled_fraction_goal=0.052,
+                recall_goal=0.360,
+                precision_goal=0.857,
+                F_score_goal=0.507,
+            )
+            check_case(
+                LopezAcosta2019(),
+                data_loader,
+                c -> (c.case_number == 61 && c.satellite == "aqua");
+                labeled_fraction_goal=0.132,
+                recall_goal=0.379,
+                precision_goal=0.754,
+                F_score_goal=0.504,
+            )
+            (; labeled_fraction, recall, precision, F_score) = run_segmentation_over_one_case(
+                data_loader,
+                c -> (c.case_number == 63 && c.satellite == "aqua"),
+                LopezAcosta2019(),
+            )
+            @test labeled_fraction ≈ 0.579 rtol = 0.1 broken = true
+            @test 0.901 ≤ recall broken = true
+            @test 0.620 ≤ precision broken = true
+            @test 0.734 ≤ F_score broken = true
+        end
         @ntestset "Image types" begin
-            region = (200:400, 500:700)
+            case = first(
+                data_loader(;
+                    case_filter=c -> (c.case_number == 6 && c.satellite == "terra")
+                ),
+            )
             supported_types = [n0f8, n6f10, n4f12, n2f14, n0f16, float32, float64]
             for target_type in supported_types
                 @info "Image type: $target_type"
@@ -35,24 +70,14 @@ using Images: segment_labels, segment_mean, labels_map
                     "./test_outputs/segmentation-LopezAcosta2019-$(target_type)-$(Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS"))";
                 )
                 segments = LopezAcosta2019()(
-                    target_type.(truecolor[region...]),
-                    target_type.(falsecolor[region...]),
-                    target_type.(landmask[region...]);
+                    target_type.(RGB.(case.modis_truecolor)),
+                    target_type.(RGB.(case.modis_falsecolor)),
+                    target_type.(RGB.(case.modis_landmask));
                     intermediate_results_callback,
                 )
                 @show segments
-                @test length(segment_labels(segments)) == 10
+                @test length(segment_labels(segments)) ≈ 65 atol = 2
             end
-        end
-        @ntestset "Medium size" begin
-            intermediate_results_callback = save_results_callback(
-                "./test_outputs/segmentation-LopezAcosta2019-medium-size-$( Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS"))";
-            )
-            segments = LopezAcosta2019()(
-                truecolor, falsecolor, landmask; intermediate_results_callback
-            )
-            @show segments
-            @test length(segment_labels(segments)) == 44
         end
     end
 end
