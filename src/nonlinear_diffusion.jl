b4139e3f2ed67f22d8aaa873ff1870e738250416
daniel watkins <danielmwatkins@gmail.com>
2025-09-04 17:07:49 -0400

    setting up diffusion functor

---
+++ src/nonlinear_diffusion.jl
@@ -0,0 +1,166 @@
+"""Nonlinear Diffusion
+
+Anisotropic diffusion was introduced by Perona and Malik (1987) and refined in subsequent publications.
+We include two implementations of the Perona and Malik algorithm: one adapted from https://github.com/Red-Portal/UltrasoundDesignGallery.jl, and the
+other a reimplementation of approach used in the default Matlab image processing toolbox.
+Since the algorithm is not truly anisotropic, we refer to it instead as nonlinear diffusion.
+
+PeronaMalikDiffusion(λ, K, niters, g_option)
+    λ = Parameter weighting the diffusion rate, needs to be between 0 and 0.25 for stability.
+    K = Numerator for the image gradient function. (TBD: Option to estimate from image gradient histogram)
+    niters = Number of interations
+    g_option = "exponential", "inverse_quadratic" (TBD: Option to provide user-defined function)
+
+
+P. Perona and J. Malik (November 1987). "Scale-space and edge detection using anisotropic diffusion". Proceedings of IEEE Computer Society Workshop on Computer Vision. pp. 16–22.
+P. Perona and J. Malik, Scale-Space and Edge Detection Using Anisotropic Diffusion, IEEE Transactions on Pattern Analysis and Machine Intelligence, 12(7):629-639, July 1990
+G. Grieg, O. Kubler, R. Kikinis, and F. A. Jolesz, Nonlinear Anisotropic Filtering of MRI Data, IEEE Transactions on Medical Imaging, 11(2):221-232, June 1992
+
+"""
+
+using Images # I think this can be taken out, I have it here for the RGB types
+abstract type AbstractDiffusionAlgorithm end
+
+@kwdef struct PeronaMalikDiffusion <: AbstractDiffusionAlgorithm
+    λ::Float64
+    K::Int
+    niters::Int
+    g::String
+    
+    # enforce conditions
+    function PeronaMalikDiffusion(λ, K, niters, g
+    )
+        !(0 < λ <= 0.25) && throw(ArgumentError("Lambda must be between zero and 0.25"))
+        K <= 0 && throw(ArgumentError("K must be greater than zero"))
+        niters <= 0 && throw(ArgumentError("Number of iterations must be greater than zero"))
+        g ∉ ["exponential", "inverse_quadratic"] && throw(ArgumentError("Unknown function name"))
+        return new(λ, K, niters, g)
+    end
+end
+
+# Default to using Perona Malik diffusion, future releases may include more modern algorithms.
+function nonlinear_diffusion(
+    img::AbstractArray{<:Union{AbstractRGB,TransparentRGB, AbstractGray}},
+    f::AbstractDiffusionAlgorithm=PeronaMalikDiffusion()
+    )
+    return f(img)
+end
+
+
+function (f::PeronaMalikDiffusion)(img::AbstractArray{<:AbstractGray})
+    
+    # future option: allow user to supply function for g
+    g(norm∇I, k) = 
+        if f.g_option == "exponential"
+            exp(-(norm∇I / k)^2)
+        elseif f.g_option == "inverse_quadratic"
+            1 / (1 + (norm∇I / k)^2)
+        end
+
+    
+    # future option: implement with 8-connectivity
+    function pmd_updater!(image, output, g, λ, k)
+        M, N = size(image)
+        padded_array = padarray(image, Pad(:replicate, 1, 1))
+
+        ∇n = padded_array[1:M, 0:N-1] .- image
+        ∇s = padded_array[1:M, 2:N+1] .- image
+        ∇e = padded_array[2:M+1, 1:N] .- image
+        ∇w = padded_array[0:M-1, 1:N] .- image
+        
+
+        Cn = g.(abs.(∇n), k)
+        Cs = g.(abs.(∇s), k)
+        Ce = g.(abs.(∇e), k)
+        Cw = g.(abs.(∇w), k)
+
+        output .= image +  λ .* (Cn .* ∇n + Cs .* ∇s + Ce .* ∇e + Cw .* ∇w)
+    end
+    
+    _img = float64.(img)
+    _out = deepcopy(_img)
+    for _ in 1:f.niters
+        # future option: estimate k from the data, as in P-M paper
+        pmd_updater!(_img, _out, g, f.λ, f.k)
+        _img, _out = _out, _img
+    end
+
+    recast_img_type = base_color_type(eltype(_out)){eltype(eltype(img))}
+    return recast_img_type.(_out)
+end
+
+function (f::PeronaMalikDiffusion)(img::AbstractArray{<:Union{AbstractRGB,TransparentRGB}})
+
+
+    # TBD
+end
+
+# Replace this with a function that broadcasts to each image channel
+function anisotropic_diffusion_3D(I)
+    rgbchannels = get_rgb_channels(I)
+
+    for i in 1:3
+        rgbchannels[:, :, i] .= anisotropic_diffusion_2D(rgbchannels[:, :, i])
+    end
+
+    return rgbchannels
+end
+
+function anisotropic_diffusion_2D(
+    # Implementation of the matlab 2D anisotropic diffusion filter default mode
+    # by Carlos Paniagua
+    I::AbstractMatrix{T}; gradient_threshold::Union{T,Nothing}=nothing, niter::Int=1
+) where {T}
+    if eltype(I) <: Int
+        I = Gray.(I ./ 255)
+    end
+
+    # Determine the gradient threshold if not provided
+    if gradient_threshold === nothing
+        dynamic_range = maximum(I) - minimum(I)
+        gradient_threshold = 0.1 * dynamic_range
+    end
+
+    # Padding the image (corrected)
+    padded_img = padarray(I, Pad(:replicate, (1, 1)))
+    dd = sqrt(2)
+    diffusion_rate = 1 / 8  # Fixed for maximal connectivity (8 neighbors)
+
+    for _ in 1:niter
+        # These are zero-indexed offset arrays
+        diff_img_north =
+            padded_img[0:(end - 1), 1:(end - 1)] .- padded_img[1:end, 1:(end - 1)]
+        diff_img_east =
+            padded_img[1:(end - 1), 1:end] .- padded_img[1:(end - 1), 0:(end - 1)]
+        diff_img_nw = padded_img[0:(end - 2), 0:(end - 2)] .- I
+        diff_img_ne = padded_img[0:(end - 2), 2:end] .- I
+        diff_img_sw = padded_img[2:end, 0:(end - 2)] .- I
+        diff_img_se = padded_img[2:end, 2:end] .- I
+
+        # Exponential conduction coefficients
+        conduct_coeff_north = exp.(-(abs.(diff_img_north) ./ gradient_threshold) .^ 2)
+        conduct_coeff_east = exp.(-(abs.(diff_img_east) ./ gradient_threshold) .^ 2)
+        conduct_coeff_nw = exp.(-(abs.(diff_img_nw) ./ gradient_threshold) .^ 2)
+        conduct_coeff_ne = exp.(-(abs.(diff_img_ne) ./ gradient_threshold) .^ 2)
+        conduct_coeff_sw = exp.(-(abs.(diff_img_sw) ./ gradient_threshold) .^ 2)
+        conduct_coeff_se = exp.(-(abs.(diff_img_se) ./ gradient_threshold) .^ 2)
+
+        # Flux calculations
+        flux_north = conduct_coeff_north .* diff_img_north
+        flux_east = conduct_coeff_east .* diff_img_east
+        flux_nw = conduct_coeff_nw .* diff_img_nw
+        flux_ne = conduct_coeff_ne .* diff_img_ne
+        flux_sw = conduct_coeff_sw .* diff_img_sw
+        flux_se = conduct_coeff_se .* diff_img_se
+
+        # Back to regular 1-indexed arrays
+        flux_north_diff = flux_north[1:(end - 1), :] .- flux_north[2:end, :]
+        flux_east_diff = flux_east[:, 2:end] .- flux_east[:, 1:(end - 1)]
+
+        # Discrete PDE solution
+        sum_ = (1 / (dd^2)) .* (flux_nw .+ flux_ne .+ flux_sw .+ flux_se)
+        I = I .+ diffusion_rate .* (flux_north_diff .- flux_north_diff .+ sum_)
+    end
+
+    return I
+end
