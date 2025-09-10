aa0fba0c463eebc35161ae4efe8b7b5f2818748c
daniel watkins <danielmwatkins@gmail.com>
2025-09-10 09:30:25 -0400

    temp fixes for image type

--- src/watershed.jl
+++ src/watershed.jl
@@ -8,6 +8,8 @@ function watershed1(bw::T) where {T<:Union{BitMatrix,AbstractMatrix{Bool}}}
     return Images.isboundary(lmap) .> 0
 end
 
+
+# dmw: Expose this function in the watershed workflow directly, using ImageMorphology functions
 function _reconst_watershed(morph_residue::Matrix{<:Integer}, se::Matrix{Bool}=se_disk20())
     mr_reconst = to_uint8(IceFloeTracker.reconstruct(morph_residue, se, "erosion", false))
     mr_reconst .= to_uint8(IceFloeTracker.reconstruct(mr_reconst, se, "dilation", true))
@@ -15,10 +17,18 @@ function _reconst_watershed(morph_residue::Matrix{<:Integer}, se::Matrix{Bool}=s
     return mr_reconst
 end
 
+# dmw: figure out new name for this that is clearer that "watershed2".
 function watershed2(morph_residue, segment_mask, ice_mask)
     # TODO: reconfigure to use async tasks or threads
     # Task 1: Reconstruct morph_residue
     # task1 = Threads.@spawn begin
+
+    # dmw: temporary fix; TBD rewrite _reconst_watershed to use ImageMorphology directly
+    maximum(morph_residue) <= 1. && (
+        morph_residue = Int64.(round.(Float64.(morph_residue) .* 255, digits=0))
+    )
+
+    # dmw: converting this to binary makes the meaning different, it's now a binary mask
     mr_reconst = _reconst_watershed(morph_residue)
     mr_reconst = ImageMorphology.local_maxima(mr_reconst; connectivity=2) .> 0
     # end
@@ -36,6 +46,6 @@ function watershed2(morph_residue, segment_mask, ice_mask)
     gmag .= impose_minima(gmag, minimamarkers)
     cc = label_components(imregionalmin(gmag), trues(3, 3))
     w = ImageSegmentation.watershed(morph_residue, cc)
-    lmap = labels_map(w)
-    return (fgm=mr_reconst, L0mask=isboundary(lmap) .> 0)
+    lmap = labels_map(w) # dmw: inspect results here. Should we not multiply by an earlier mask to remove weird edges?
+    return (fgm=mr_reconst, L0mask=isboundary(lmap) .> 0) # dmw: what does fgm stand for?
 end
