4161aa6a004cffd3ace28cacfeea82a8861a5e34
John Gerrard Holland <john_holland1@brown.edu>
2025-09-15 18:59:48 +0000

    fix tests of dummy functions

--- test/test-dummy.jl
+++ test/test-dummy.jl
@@ -1,9 +1,9 @@
 @testitem "dummy functions" begin
     # TODO: remove once dummy functions are deleted
-    @test IceFloeTracker.Filtering.dummy_filtering_function()
-    @test IceFloeTracker.Morphology.dummy_morphology_function()
-    @test IceFloeTracker.Preprocessing.dummy_preprocessing_function()
-    @test IceFloeTracker.Segmentation.dummy_segmentation_function()
-    @test IceFloeTracker.Tracking.dummy_tracking_function()
-    @test IceFloeTracker.Utils.dummy_utils_function()
+    @test !isempty(IceFloeTracker.Filtering.dummy_filtering_function())
+    @test !isempty(IceFloeTracker.Morphology.dummy_morphology_function())
+    @test !isempty(IceFloeTracker.Preprocessing.dummy_preprocessing_function())
+    @test !isempty(IceFloeTracker.Segmentation.dummy_segmentation_function())
+    @test !isempty(IceFloeTracker.Tracking.dummy_tracking_function())
+    @test !isempty(IceFloeTracker.Utils.dummy_utils_function())
 end
