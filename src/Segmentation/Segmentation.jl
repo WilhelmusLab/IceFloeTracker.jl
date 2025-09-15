11256c0e79c68ee6ddb5069cc463fcb3cbb29e2d
John Gerrard Holland <john_holland1@brown.edu>
2025-09-15 14:25:31 +0000

    add todo-delete notes to dummy functions

--- src/Segmentation/Segmentation.jl
+++ src/Segmentation/Segmentation.jl
@@ -4,6 +4,10 @@ module Segmentation
     dummy_segmentation_function()
 
 Example function for segmentation module
+
+!!! todo "Delete this once real functions are added"
+    This function should be removed when real segmentation functions are moved into this module.
+
 """
 function dummy_segmentation_function()
     return "This is a dummy segmentation function."
--- src/Utils/Utils.jl
+++ src/Utils/Utils.jl
@@ -4,6 +4,10 @@ module Utils
     dummy_utils_function()
 
 Example function for utils module
+
+!!! todo "Delete this once real functions are added"
+    This function should be removed when real utils functions are moved into this module.
+
 """
 function dummy_utils_function()
     return "This is a dummy utils function."
