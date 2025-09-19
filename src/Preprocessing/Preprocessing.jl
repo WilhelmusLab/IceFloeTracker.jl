2cacc4ad90690f99595e850645d36b347f24de49
daniel watkins <danielmwatkins@gmail.com>
2025-09-19 11:14:14 -0400

    Initial commit

--- src/Preprocessing/Preprocessing.jl
+++ src/Preprocessing/Preprocessing.jl
@@ -1,17 +1,53 @@
 module Preprocessing
 
-"""
-    dummy_preprocessing_function()
+abstract type IceFloePreprocessingAlgorithm end
 
-Example function for preprocessing module
-
-!!! todo "Delete this once real functions are added"
-    This function should be removed when real preprocessing functions are moved into this module.
-"""
-function dummy_preprocessing_function()
-    return "This is a dummy preprocessing function."
+struct LopezAcostaPreprocessing2019 <: IceFloePreprocessingAlgorithm
+    param1
+    param2
 end
 
-export dummy_preprocessing_function
+struct WatkinsPreprocessing2025 <: IceFloePreprocessingAlgorithm
+    param1
+    param2
+end
+
+function WatkinsPreprocessing2025(; landmask_structuring_element=make_landmask_se())
+    return WatkinsPreprocessing2025(landmask_structuring_element)
+end
+
+"""
+Produce a grayscale image with sharpened floe edges and smoothed floe interiors, following Watkins et al. 2025.
+"""
+function (p::WatkinsPreprocessing2025)
+    (truecolor_image,
+     falsecolor_image,
+     cloud_mask,
+     land_mask,
+     tiles)
+    # image diffusion
+    # equalization
+    # sharpening (local)
+    # morphological residue
+    # gamma adjustment
+    # sharpening (regional)
+end
+
+
+function LopezAcostaPreprocessing2019(; landmask_structuring_element=make_landmask_se())
+    return LopezAcosta2019(landmask_structuring_element)
+end
+
+function (p::LopezAcostaPreprocessing2019)()
+    # initial algorithm steps
+end
+
+
+
+
+
+
+export LopezAcostaPreprocessing2019
+export WatkinsPreprocessing2025
 
 end
