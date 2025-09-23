d6c9514d4546064b4265a3935bd1a19583c0dab8
Daniel Watkins <danielmewatkins@gmail.com>
2025-09-23 16:56:40 -0400

    fix filenames

--- test/test-create-landmask.jl
+++ test/test-create-landmask.jl
@@ -6,29 +6,19 @@
     struct_elem = readdlm(strel_file, ',', Bool) # read in original matlab structuring element -  a disk-shaped kernel with radius of 50 px
     matlab_landmask_dilated = convert(BitMatrix, load("$(test_data_dir)/matlab_landmask_dilated.png")[lm_test_region...])
     matlab_landmask_non_dilated = convert(BitMatrix, load("$(test_data_dir)/matlab_landmask_non_dilated.png")[lm_test_region...])
-    lm_image = load(landmask_file)
     test_image = load(truecolor_test_image_file)[lm_test_region...]
-    
-    landmask_image = load("$(test_data_dir)/landmask.tiff")
+    lm_image = load("$(test_data_dir)/landmask.tiff")[lm_test_region...]
 
     # Check whether the built-in strel matches the matlab strel
     @time landmask_init_strel = IceFloeTracker.create_landmask(lm_image[lm_test_region...], struct_elem)
     @time landmask_default_strel = IceFloeTracker.create_landmask(lm_image[lm_test_region...])
 
     @test landmask_default_strel == landmask_init_strel 
-       
-    # Create and save the landmask images
-    landmask_imgs = IceFloeTracker.create_landmask(lm_image)
-#   IceFloeTracker.@persist landmask_imgs.non_dilated "test/test_inputs/julia_landmask_non_dilated.png"
-#   IceFloeTracker.@persist landmask_imgs.dilated "test/test_inputs/julia_landmask_dilated.png"
-
-    # Generate testing files
-    @time landmask_no_dilate = landmask.non_dilated
+    
+    landmask = landmask_default_strel
 
     @time masked_image = IceFloeTracker.apply_landmask(test_image, landmask.dilated)
-    @time masked_image_no_dilate = IceFloeTracker.apply_landmask(
-        test_image, landmask_no_dilate
-    )
+    @time masked_image_no_dilate = IceFloeTracker.apply_landmask(test_image, landmask.non_dilated)
 
     # test for percent difference in landmask images
     @test test_similarity(landmask.dilated, convert(BitMatrix, matlab_landmask_dilated), 0.005)
@@ -54,7 +44,7 @@
         "$(test_output_dir)/matlab_landmask_test_" *
         Dates.format(Dates.now(), "yyyy-mm-dd-HHMMSS") *
         ".png"
-    IceFloeTracker.@persist matlab_landmask matlab_landmask_filename
+    IceFloeTracker.@persist matlab_landmask_dilated matlab_landmask_filename
 
     landmask_filename =
         "$(test_output_dir)/landmask_test_" *
