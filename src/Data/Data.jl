68286e90f5d9c9f7c67c68df44f60e8f6af86c47
John Gerrard Holland <john_holland1@brown.edu>
2025-10-01 21:01:24 +0000

    update docstring for data loader

--- src/Data/Data.jl
+++ src/Data/Data.jl
@@ -38,61 +38,65 @@ Loader for validated ice floe data.
 abstract type ValidationDataLoader end
 
 """
-Loader for validated ice floe data structured like https://github.com/danielmwatkins/ice_floe_validation_dataset.
 
-    Struct fields:
-    - `url`: URL of the GitHub repository with the dataset
-    - `ref`: `git` ref of the commit from which to load the data
-    - `dataset_metadata_path`: path within the repository to a CSV file describing the data
-    - `cache_dir`: local path where the data will be stored.
+    Watkins2025GitHub(; ref)()
+    Watkins2025GitHub(; ref, [url, dataset_metadata_path, cache_dir])(; [case_filter])
 
+Loader for validated ice floe data from https://github.com/danielmwatkins/ice_floe_validation_dataset.
+
+Struct fields: 
+- `url`: URL of the GitHub repository with the dataset
+- `ref`: `git` ref of the commit from which to load the data
+- `dataset_metadata_path`: path within the repository to a CSV file describing the data
+- `cache_dir`: local path where the data will be stored.
+
+
+Cacheing: 
+Data are downloaded to the `ref` subdirectory of `cache_dir`, e.g. /tmp/Watkins2025/main`. 
+If a file of the correct name already exists in that path, if loaded again the cached data will be returned.
+If the data change in the source for that ref, the loader won't load the new data.
+In that case, it's necessary to delete the cached file.
+A better choice is to use a specific revision `ref`: either a tag, or a commit ID.
+
+Function arguments:
+- `case_filter`: function run on each metadata entry; 
+    if it returns true, then the data from that case is returned 
+
+Function returns a named tuple with these fields:
+- `metadata`: DataFrame of the cases which passed the `case_filter`
+- `data`: Generator which returns a `ValidationDataCase` for each case which passed the `case_filter`
     
-    Cacheing: 
-    Data are downloaded to the `ref` subdirectory of `cache_dir`, e.g. /tmp/Watkins2025/main`. 
-    If a file of the correct name already exists in that path, if loaded again the cached data will be returned.
-    If the data change in the source for that ref, the loader won't load the new data.
-    In that case, it's necessary to delete the cached file.
-    A better choice is to use a specific revision `ref`: either a tag, or a commit ID.
+Example:
+```jldoctest; setup = :(using IceFloeTracker)
+julia> data_loader = Watkins2025GitHub(; ref="a451cd5e62a10309a9640fbbe6b32a236fcebc70")
+julia> dataset = data_loader(;case_filter=c -> (
+                        c.visible_floes == "yes" &&
+                        c.cloud_category_manual == "none" &&
+                        c.artifacts == "no"
+                    ));
+julia> dataset.metadata
+8×28 DataFrame
+    Row │ case_number  region        start_date  center_lon  center_lat  center_x  center_y  month  sea_ice_fr ⋯
+        │ Int64        String31      Date        Float64     Float64     Int64     Int64     Int64  Float64    ⋯
+   ─────┼───────────────────────────────────────────────────────────────────────────────────────────────────────
+      1 │          11  baffin_bay    2011-07-02    -70.7347     72.3303   -837500  -1737500      7             ⋯
+      2 │          14  baffin_bay    2022-07-06    -69.0755     72.3157   -787500  -1762500      7
+      3 │          48  beaufort_sea  2021-04-27   -140.612      70.1346  -2162500    212500      4
+      4 │          48  beaufort_sea  2021-04-27   -140.612      70.1346  -2162500    212500      4
+      5 │          54  beaufort_sea  2015-05-16   -136.675      70.4441  -2137500     62500      5             ⋯
+      6 │          54  beaufort_sea  2015-05-16   -136.675      70.4441  -2137500     62500      5
+      7 │         128  hudson_bay    2019-04-15    -91.9847     57.853   -2612500  -2437500      4
+      8 │         166  laptev_sea    2016-09-04    136.931      79.7507    -37500   1112500      9
+                                                                                        20 columns omitted
 
-    Function arguments:
-    - `case_filter`: function run on each metadata entry; 
-      if it returns true, then the data from that case is returned 
+julia> first(dataset.data)
+ValidationDataCase("011-baffin_bay-100km-20110702-aqua-250m", Dict{Symbol, Any}(:sea_ice_fraction => 0.8, :vi...
 
-    Function returns a named tuple with these fields:
-    - `metadata`: DataFrame of the cases which passed the `case_filter`
-    - `data`: Generator which returns a `ValidationDataCase` for each case which passed the `case_filter`
-        
-    Example:
-    ```
-    julia> data_loader = Watkins2025GitHub(; ref="a451cd5e62a10309a9640fbbe6b32a236fcebc70")
-    julia> dataset = data_loader(;case_filter=c -> (
-                            c.visible_floes == "yes" &&
-                            c.cloud_category_manual == "none" &&
-                            c.artifacts == "no"
-                        ));
-    julia> dataset.metadata
-    8×28 DataFrame
-     Row │ case_number  region        start_date  center_lon  center_lat  center_x  center_y  month  sea_ice_fr ⋯
-         │ Int64        String31      Date        Float64     Float64     Int64     Int64     Int64  Float64    ⋯
-    ─────┼───────────────────────────────────────────────────────────────────────────────────────────────────────
-       1 │          11  baffin_bay    2011-07-02    -70.7347     72.3303   -837500  -1737500      7             ⋯
-       2 │          14  baffin_bay    2022-07-06    -69.0755     72.3157   -787500  -1762500      7
-       3 │          48  beaufort_sea  2021-04-27   -140.612      70.1346  -2162500    212500      4
-       4 │          48  beaufort_sea  2021-04-27   -140.612      70.1346  -2162500    212500      4
-       5 │          54  beaufort_sea  2015-05-16   -136.675      70.4441  -2137500     62500      5             ⋯
-       6 │          54  beaufort_sea  2015-05-16   -136.675      70.4441  -2137500     62500      5
-       7 │         128  hudson_bay    2019-04-15    -91.9847     57.853   -2612500  -2437500      4
-       8 │         166  laptev_sea    2016-09-04    136.931      79.7507    -37500   1112500      9
-                                                                                            20 columns omitted
-
-    julia> first(dataset.data)
-    ValidationDataCase("011-baffin_bay-100km-20110702-aqua-250m", Dict{Symbol, Any}(:sea_ice_fraction => 0.8, :vi...
-
-    julia> first(dataset.data).validated_labeled_floes
-    Segmented Image with:
-    labels map: 400×400 Matrix{Int64}
-    number of labels: 105
-    ```
+julia> first(dataset.data).validated_labeled_floes
+Segmented Image with:
+labels map: 400×400 Matrix{Int64}
+number of labels: 105
+```
 """
 @kwdef struct Watkins2025GitHub <: ValidationDataLoader
     ref::AbstractString
