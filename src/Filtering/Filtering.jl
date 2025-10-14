41e3ec45e49eab6a6fc70ad6185cf63e5146c956
John Gerrard Holland <john_holland1@brown.edu>
2025-10-14 20:08:02 +0000

    remove dummy_filtering_function

--- src/Filtering/Filtering.jl
+++ src/Filtering/Filtering.jl
@@ -1,17 +1,4 @@
 module Filtering
-"""
-    dummy_filtering_function()
 
-Example function for filtering module
-
-!!! todo "Delete this once real functions are added"
-    This function should be removed when real filtering functions are moved into this module.
-
-"""
-function dummy_filtering_function()
-    return "This is a dummy filtering function."
-end
-
-export dummy_filtering_function
 
 end
