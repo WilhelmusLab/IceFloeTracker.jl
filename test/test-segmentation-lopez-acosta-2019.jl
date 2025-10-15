b21aa2ff4c7fac7bd7e46dd4bb0669aa7b1742ec
John Gerrard Holland <john_holland1@brown.edu>
2025-10-15 16:28:33 +0000

    switch using for import

--- test/test-segmentation-lopez-acosta-2019.jl
+++ test/test-segmentation-lopez-acosta-2019.jl
@@ -1,5 +1,5 @@
 @testitem "LopezAcosta2019 – simple case" tags = [:e2e, :smoke] begin
-    using DataFrames: DataFrame, nrow
+    import DataFrames: DataFrame, nrow
     data_loader = Watkins2025GitHub(; ref="a451cd5e62a10309a9640fbbe6b32a236fcebc70")
 
     case = first(data_loader(c -> (c.case_number == 6 && c.satellite == "terra")))
