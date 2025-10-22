35f4032ab813618637cf040db9024da4c94843f2
John Gerrard Holland <john_holland1@brown.edu>
2025-10-22 13:04:57 +0000

    add linux-specific setup step

--- .devcontainer/PyCallSetup.jl/setup.jl
+++ .devcontainer/PyCallSetup.jl/setup.jl
@@ -16,10 +16,12 @@ ENV["PYTHON"] = ""
 Pkg.add("PyCall")
 Pkg.build("PyCall")
 
-# Fix an error which occurs for some versions of Conda
+# Fix an error which occurs for some versions of Conda on Linux
 # where scipy can't be imported.
 # Inspired by:
 # https://discourse.julialang.org/t/version-cxxabi-1-3-15-not-found-when-loading-matplotlib-through-pythoncall/131671/12?page=2
 Pkg.add("Conda")
 using Conda
-Conda.add("libstdcxx<14.0")
+if Sys.islinux()
+    Conda.add("libstdcxx<14.0")
+end
