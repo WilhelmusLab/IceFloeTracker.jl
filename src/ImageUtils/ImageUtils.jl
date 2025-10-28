90879ed869b288911564db2f621452c095f136cf
John Gerrard Holland <john_holland1@brown.edu>
2025-10-28 13:32:42 +0000

    refactor: move brighten to ImageUtils

--- src/ImageUtils/ImageUtils.jl
+++ src/ImageUtils/ImageUtils.jl
@@ -3,8 +3,9 @@ Module for general image utilities.
 """
 module ImageUtils
 
-export masker, apply_mask
+export masker, apply_mask, get_brighten_mask, imbrighten
 
 include("mask.jl")
+include("brighten.jl")
 
 end
