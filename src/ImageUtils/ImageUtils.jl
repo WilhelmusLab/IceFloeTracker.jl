dca36d067d26341b9e5790361eb84bb99d7a0cb3
John Gerrard Holland <john_holland1@brown.edu>
2025-10-28 12:48:22 +0000

    refactor: move imcomplement to ImageUtils

--- src/ImageUtils/ImageUtils.jl
+++ src/ImageUtils/ImageUtils.jl
@@ -3,8 +3,9 @@ Module for general image utilities.
 """
 module ImageUtils
 
-export masker, apply_mask
+export masker, apply_mask, imcomplement
 
 include("mask.jl")
+include("imcomplement.jl")
 
 end
