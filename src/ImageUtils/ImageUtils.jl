5e701706539faa2a8a4b686ba40c187da03aaf6d
John Gerrard Holland <john_holland1@brown.edu>
2025-10-28 13:39:06 +0000

    refactor: move tiling functions to ImageUtils

--- src/ImageUtils/ImageUtils.jl
+++ src/ImageUtils/ImageUtils.jl
@@ -3,8 +3,9 @@ Module for general image utilities.
 """
 module ImageUtils
 
-export masker, apply_mask
+export masker, apply_mask, get_area_missed, get_optimal_tile_size, get_tile_dims, get_tiles
 
 include("mask.jl")
+include("tiling.jl")
 
 end
