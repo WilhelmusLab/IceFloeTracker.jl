ce9d58212c5c323e3e47dbfff5d0656f2b601d99
John Gerrard Holland <john_holland1@brown.edu>
2025-10-28 19:35:43 +0000

    move sharpening to Filtering, not ImageUtils

--- src/ImageUtils/ImageUtils.jl
+++ src/ImageUtils/ImageUtils.jl
@@ -3,11 +3,10 @@ Module for general image utilities.
 """
 module ImageUtils
 
-export masker, apply_mask, imcomplement, to_uint8, imsharpen, unsharp_mask
+export masker, apply_mask, imcomplement, to_uint8
 
 include("imcomplement.jl")
 include("mask.jl")
-include("sharpen.jl")
 include("uint8.jl")
 
 end
