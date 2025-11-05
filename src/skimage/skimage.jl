b2ffc23726cae36784610eccc5d309b01896785b
John Gerrard Holland <john_holland1@brown.edu>
2025-11-05 19:57:51 +0000

    refactor: move skimage.measure into a separate module

--- src/skimage/skimage.jl
+++ src/skimage/skimage.jl
@@ -2,16 +2,59 @@ module skimage
 
 using PythonCall
 
-export sk_measure, sk_morphology, sk_exposure
-
-const sk_measure = PythonCall.pynew()
+export sk_morphology, sk_exposure, measure
 const sk_morphology = PythonCall.pynew()
 const sk_exposure = PythonCall.pynew()
 
 function __init__()
-    PythonCall.pycopy!(sk_measure, pyimport("skimage.measure"))
     PythonCall.pycopy!(sk_morphology, pyimport("skimage.morphology"))
     PythonCall.pycopy!(sk_exposure, pyimport("skimage.exposure"))
+    return nothing
 end
+
+module measure
+
+    export regionprops_table
+
+    using PythonCall
+    import DataFrames: DataFrame
+
+    const _measure = PythonCall.pynew()
+    function __init__()
+        PythonCall.pycopy!(_measure, PythonCall.pyimport("skimage.measure"))
+        return nothing
+    end
+
+    """Wrapper around the python `skimage.measure.regionprops_table` function."""
+    function regionprops_table(
+        label_img::Matrix{<:Integer},
+        intensity_img::Union{Nothing,AbstractMatrix}=nothing;
+        properties::Union{Vector{<:AbstractString},Tuple{String,Vararg{String}}}=(
+            "centroid",
+            "area",
+            "major_axis_length",
+            "minor_axis_length",
+            "convex_area",
+            "bbox",
+            "perimeter",
+            "orientation",
+        ),
+    )::DataFrame
+        result =
+            _measure.regionprops_table(
+                PythonCall.Py(label_img).to_numpy(),
+                PythonCall.Py(intensity_img).to_numpy();
+                properties,
+            ) |>
+            (x -> PythonCall.pyconvert(Dict, x)) |>
+            DataFrame
+
+        return result
+    end
+end
+
+module morphology end
+
+module exposure end
 
 end
