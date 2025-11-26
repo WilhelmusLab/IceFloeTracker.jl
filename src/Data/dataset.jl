9bde5b9b354c556af12761b053c8a079ab6e7f59
John Gerrard Holland <john_holland1@brown.edu>
2025-11-26 20:40:28 +0000

    add new sort syntax

--- src/Data/dataset.jl
+++ src/Data/dataset.jl
@@ -30,26 +30,34 @@ function loader(ds::Dataset)::AbstractLoader
 end
 
 Base.length(ds::Dataset)::Int = nrow(info(ds))
+
+Base.getindex(ds::Dataset, i::Int) = Case(ds.loader, ds.info[i, :])
+
 Base.iterate(ds::Dataset) = iterate(Case.(Ref(ds.loader), eachrow(info(ds))))
 function Base.iterate(ds::Dataset, state)
     return iterate(Case.(Ref(loader(ds)), eachrow(info(ds))), state)
 end
+
 Base.filter(f::Function, ds::Dataset) = Dataset(ds.loader, filter(row -> f(row), ds.info))
 Base.filter!(f::Function, ds::Dataset) = filter!(row -> f(row), ds.info)
 
-Base.getindex(ds::Dataset, i::Int) = Case(ds.loader, ds.info[i, :])
-function Base.sort(ds::Dataset, args...; kwargs...)
-    return Dataset(ds.loader, sort(ds.info, args...; kwargs...))
-end
-Base.sort!(ds::Dataset, args...; kwargs...) = sort!(ds.info, args...; kwargs...)
-function Base.sort(ds::Dataset, args...; kwargs...)
-    return Dataset(ds.loader, sort(ds.info, args...; kwargs...))
-end
-Base.sort!(ds::Dataset, args...; kwargs...) = sort!(ds.info, args...; kwargs...)
-Base.sort!(cols::Vector{Symbol}, ds::Dataset; kwargs...) = sort!(ds.info, cols; kwargs...)
-Base.reverse(ds::Dataset) = Dataset(ds.loader, reverse(ds.info))
-Base.reverse!(ds::Dataset) = reverse!(ds.info)
 function subset(ds::Dataset, args...; kwargs...)
     return Dataset(ds.loader, subset(ds.info, args...; kwargs...))
 end
 subset!(ds::Dataset, args...; kwargs...) = subset!(ds.info, args...; kwargs...)
+
+function Base.sort(ds::Dataset, args...; kwargs...)
+    return Dataset(ds.loader, sort(ds.info, args...; kwargs...))
+end
+function Base.sort(ds::Dataset, args...; kwargs...)
+    return Dataset(ds.loader, sort(ds.info, args...; kwargs...))
+end
+function Base.sort(cols::Vector{Symbol}, ds::Dataset; kwargs...)
+    return Dataset(ds.loader, sort(cols, ds.info; kwargs...))
+end
+Base.sort!(cols::Vector{Symbol}, ds::Dataset; kwargs...) = sort!(ds.info, cols; kwargs...)
+Base.sort!(ds::Dataset, args...; kwargs...) = sort!(ds.info, args...; kwargs...)
+
+Base.reverse(ds::Dataset) = Dataset(ds.loader, reverse(ds.info))
+Base.reverse!(ds::Dataset) = reverse!(ds.info)
+
