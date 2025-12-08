77b440c4ccdf05c445218ad2830f233cfeeb466f
daniel watkins <danielmwatkins@gmail.com>
2025-12-08 12:01:25 -0500

    updated test

--- test/test-conditional-adaptive-histeq.jl
+++ test/test-conditional-adaptive-histeq.jl
@@ -36,16 +36,6 @@
 end
 
 @testitem "conditional adaptivehisteq (data loader)" setup = [FalseColorCloudmask] begin
-<<<<<<< HEAD
-    dataset = filter(
-        c -> c.case_number == 161 && c.satellite == "terra", Watkins2026Dataset()
-    )
-    case = first(dataset)
-    false_color_image = modis_falsecolor(case)
-    true_color_image = modis_truecolor(case)
-    landmask = modis_landmask(case)
-
-=======
     dataset = Watkins2026GitHub()(;
         case_filter=c -> c.case_number == 161 && c.satellite == "terra"
     )
@@ -54,7 +44,6 @@ end
     false_color_image = RGB.(modis_falsecolor(case))
     landmask = RGB.(modis_landmask(case))
     
->>>>>>> 868770a4 (update to 2026 loader)
     clouds = _get_false_color_cloudmasked(;
         false_color_image=false_color_image,
         prelim_threshold=110.0,
