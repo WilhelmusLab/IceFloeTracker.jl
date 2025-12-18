fcf5cde7fee67c02d19b8769d947496318d67e92
Daniel Watkins <danielmewatkins@gmail.com>
2025-12-18 15:36:27 -0500

    remove area 1 pixel from test

--- test/test-regionprops.jl
+++ test/test-regionprops.jl
@@ -7,24 +7,24 @@
     bw_img = Bool.(rand([0, 1], 5, 10))
     bw_img[end, 7] = 1
     label_img = label_components(bw_img, trues(3, 3))
-    properties = (
+    properties = [
         "label",
         "centroid",
         "area",
         "major_axis_length",
         "minor_axis_length",
-        "convex_area",
         "bbox",
         "perimeter",
-        "orientation",
-    )
+        "orientation"
+    ]
     extra_props = nothing
-    table = regionprops_table(label_img, bw_img; properties=properties)
-    total_labels = maximum(label_img)
 
+    table = regionprops_table(label_img, bw_img; properties=properties, minimum_area=1)
+    total_labels = maximum(label_img)
+    
     # Tests for regionprops_table
     @test typeof(table) <: DataFrame # check correct data type
-    @test nrow(table) == (total_labels) # check correct table size
+    @test nrow(table) == (total_labels - 1) # One point has area 1, we exclude that point intentionally
     @test all(
         [
             "area",
@@ -34,7 +34,7 @@
             "max_col",
             "row_centroid",
             "col_centroid",
-            "convex_area",
+            #"convex_area",
             "label",
             "major_axis_length",
             "minor_axis_length",
@@ -56,9 +56,7 @@
     # Tests for regionprops
     regions = regionprops(label_img, bw_img)
 
-    # Check some data matches in table
-    randnum = rand(1:total_labels)
-    @test table.area[randnum] == regions[randnum].area
+    @test table.area[1] == regions[1].area
 
     # Check floe masks generation and correct cropping
     add_floemasks!(table, label_img)
@@ -66,6 +64,6 @@
         [
             length(unique(label_components(table.mask[i], trues(3, 3)))) for
             i in 1:nrow(table)
-        ] .== [2, 2, 1],
+        ] .== [2, 2],
     )
 end
