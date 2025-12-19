8ec7df1aceae9451714cad98b73b1371bbec27de
Daniel Watkins <danielmwatkins@gmail.com>
2025-12-19 17:28:37 -0500

    Update src/skimage/skimage.jl
    Co-authored-by: Carlos Paniagua <cpaniaguam@gmail.com>

--- src/skimage/skimage.jl
+++ src/skimage/skimage.jl
@@ -9,7 +9,6 @@ const sk_exposure = PyNULL()
 
 function __init__()
     skimage = "scikit-image=0.25.2"
-    # copy!(sk_measure, pyimport_conda("skimage.measure", skimage))
     copy!(sk_exposure, pyimport_conda("skimage.exposure", skimage))
     copy!(sk_morphology, pyimport_conda("skimage.morphology", skimage))
     return nothing
