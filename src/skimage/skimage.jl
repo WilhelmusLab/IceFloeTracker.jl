c0551fff0086de391b6ccedf799c094162659417
Daniel Watkins <danielmewatkins@gmail.com>
2025-12-19 10:05:05 -0500

    clearing unused functions

--- src/skimage/skimage.jl
+++ src/skimage/skimage.jl
@@ -2,15 +2,16 @@ module skimage
 
 using PyCall
 
-export sk_measure, sk_morphology, sk_exposure
+# export sk_measure, sk_morphology, sk_exposure
+export sk_morphology, sk_exposure
 
-const sk_measure = PyNULL()
+# const sk_measure = PyNULL()
 const sk_morphology = PyNULL()
 const sk_exposure = PyNULL()
 
 function __init__()
     skimage = "scikit-image=0.25.2"
-    copy!(sk_measure, pyimport_conda("skimage.measure", skimage))
+    # copy!(sk_measure, pyimport_conda("skimage.measure", skimage))
     copy!(sk_exposure, pyimport_conda("skimage.exposure", skimage))
     copy!(sk_morphology, pyimport_conda("skimage.morphology", skimage))
     return nothing
