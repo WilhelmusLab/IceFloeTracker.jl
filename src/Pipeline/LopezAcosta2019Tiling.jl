5e5ff9f5e7cc30dab9360bb325ea96401ebbaa86
John Gerrard Holland <john_holland1@brown.edu>
2026-01-16 14:34:51 +0000

    remove remaining sk_morphology code

--- src/Pipeline/LopezAcosta2019Tiling.jl
+++ src/Pipeline/LopezAcosta2019Tiling.jl
@@ -33,7 +33,6 @@ import Images:
     SegmentedImage,
     segment_mean,
     Equalization
-import ..skimage: sk_morphology
 import ..ImageUtils: get_brighten_mask, to_uint8, imcomplement, imbrighten, get_tiles
 import ..Filtering: unsharp_mask, conditional_histeq, rgb2gray, imgradientmag
 import ..Preprocessing:
