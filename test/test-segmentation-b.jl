7eaa548f66e3f2423d6d311dc7c07243d24265a9
Daniel Watkins <danielmewatkins@gmail.com>
2026-02-07 17:46:01 -0500

    update seg b test

--- test/test-segmentation-b.jl
+++ test/test-segmentation-b.jl
@@ -1,5 +1,5 @@
 @testitem "Segmentation-B" begin
-    using Images: @test_approx_eq_sigma_eps, load, float64, strel_diamond, closing
+    using Images: @test_approx_eq_sigma_eps, load, float64, strel_diamond, closing, clamp01nan
     import Statistics: mean
     include("config.jl")
     include("test_error_rate.jl")
@@ -12,14 +12,18 @@
     )
     matlab_brightened = float64.(load("$(test_data_dir)/matlab_I.png"))
 
-    # @time segB = LopezAcosta2019.segmentation_B(
-    #     sharpened_image, .!cloudmask, segmented_a_ice_mask
-    # )
 
-    threshold_mask = sharpened_image .> 0.4
-    brightened_image = (sharpened_image .* 1.3) .* threshold_mask
-    clamp!(brightened_image, 0, 1)
-  
+    # TBD: Update the segB function to match this exactly.
+    not_ice_mask = deepcopy(sharpened_image)
+    not_ice_mask[not_ice_mask .< 0.4] .= 0
+    not_ice_bit = not_ice_mask .* 0.3
+    not_ice_mask .= not_ice_bit .+ sharpened_image
+    brightened_image = map(clamp01nan, not_ice_mask)
+
+
+    # threshold_mask = sharpened_image .> 0.4
+    # brightened_image = (sharpened_image .* 1.3) .* threshold_mask
+    # clamp!(brightened_image, 0, 1)
     segB = LopezAcosta2019.segB_binarize(sharpened_image, brightened_image, cloudmask)
     
     @info "Merging segmentation results"
@@ -34,8 +38,8 @@
     # Temporary replacement of the test approx eq. The brightened image does look different than
     # the matlab image, and it's mainly in ocean areas where the clamped image is darker. 
     # It's worth checking how much differs with the choice of how to clamp / rescale intensity.
-    # @test (@test_approx_eq_sigma_eps brightened_image matlab_brightened [0, 0] 0.02) ===
-    #     nothing # Images differ in the 
+    @test (@test_approx_eq_sigma_eps brightened_image matlab_brightened [0, 0] 0.02) ===
+        nothing # Images differ in the 
 
     @test mean(abs.(vec(Float64.(brightened_image) .- Float64.(matlab_brightened)))) < 0.04
     @test typeof(segAB_intersect) == typeof(matlab_ice_intersect)
