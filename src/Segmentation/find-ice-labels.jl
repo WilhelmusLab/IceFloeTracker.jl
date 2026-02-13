fadf4a48bb88b926e893502958bb71b05e4a27c9
daniel watkins <danielmwatkins@gmail.com>
2026-02-13 16:47:50 -0500

    warn with get ice peaks error

--- src/Segmentation/find-ice-labels.jl
+++ src/Segmentation/find-ice-labels.jl
@@ -39,12 +39,17 @@ function get_ice_peaks(
     # we don't want to include in the normalization. If no potential
     # ice pixels, then return early
     counts = normalizer > 0 ? counts ./ normalizer : return Inf
-    pks = findmaxima(counts, window_size) |> peakproms! |> peakwidths!
-    pks_df = DataFrame(pks[Not(:data)])
-    pks_df = sort(pks_df, :proms; rev=true)
-    mx, argmx = findmax(pks_df.proms)
-    mx < minimum_prominence && return Inf
-    return edges[pks_df[argmx, :indices]]
+    try
+        pks = findmaxima(counts, window_size) |> peakproms! |> peakwidths!
+        pks_df = DataFrame(pks[Not(:data)])
+        pks_df = sort(pks_df, :proms; rev=true)
+        mx, argmx = findmax(pks_df.proms)
+        mx < minimum_prominence && return Inf
+        return edges[pks_df[argmx, :indices]]
+    catch
+        @warn "Peaks.findmaxima failed"
+        return Inf
+    end
 end
 
 """
