f08a7b90c7c00a645eeec095e3a9fd71ab1e4be6
John Gerrard Holland <john_holland1@brown.edu>
2026-02-17 21:07:06 +0000

    return nothing instead of a boolean expression

--- src/Filtering/clahe.jl
+++ src/Filtering/clahe.jl
@@ -144,8 +144,9 @@ end
 
 function validate_parameters(f::ContrastLimitedAdaptiveHistogramEqualization)
     # !(0 <= f.clip <= 1) && throw(ArgumentError("The parameter `clip` must be in the range [0..1]."))
-    return !(1 <= f.rblocks && 1 <= f.cblocks) &&
-           throw(ArgumentError("At least 1 contextual regions required (1x1 or greater)."))
+    !(1 <= f.rblocks && 1 <= f.cblocks) &&
+        throw(ArgumentError("At least 1 contextual regions required (1x1 or greater)."))
+    return nothing
 end
 
 function redistribute_histogram(
