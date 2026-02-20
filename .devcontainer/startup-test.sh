ec617ea6e1832fa34ce8aaf806df4ecda910ece2
John Gerrard Holland <john_holland1@brown.edu>
2026-02-20 17:21:25 +0000

    remove cd to unbound variable

--- .devcontainer/startup-test.sh
+++ .devcontainer/startup-test.sh
@@ -2,6 +2,5 @@
 
 set -xeuo pipefail
 
-cd "${CODESPACE_VSCODE_FOLDER}"  # Ensure execution in the root directory of the repository
 julia -e 'using Pkg; Pkg.add("TestEnv")'  # Install TestEnv globally
 julia --project=. -e 'using TestEnv; TestEnv.activate(); include("test/runtests-smoke.jl")'
