4026ca952304c811dbf5eaa3ec61236aa7c6e1e6
John Gerrard Holland <john_holland1@brown.edu>
2026-02-26 15:26:39 +0000

    add pandas to snakemake dependencies

--- .devcontainer/setup-workflow-dependencies.sh
+++ .devcontainer/setup-workflow-dependencies.sh
@@ -4,5 +4,6 @@ set -xeuo pipefail
 sudo apt update
 sudo apt install -y libgl1 libgdal-dev libglib2.0-0 -y
 
-pipx install snakemake 
-pipx inject snakemake pyproj
+# Set up Snakemake and tools needed in the snakemake workflow
+pipx install snakemake
+pipx inject snakemake pyproj pandas
