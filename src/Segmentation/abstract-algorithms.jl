32fa6212ea21466b6ce92374171155d9fcb3d15b
John Gerrard Holland <john_holland1@brown.edu>
2026-03-19 19:35:13 +0000

    Refactor Pipeline function signatures for consistency

--- src/Segmentation/abstract-algorithms.jl
+++ src/Segmentation/abstract-algorithms.jl
@@ -1,1 +1,26 @@
 abstract type IceFloeSegmentationAlgorithm end
+
+using Images: AbstractRGB, TransparentRGB, Gray, float64
+using ..Preprocessing: create_landmask
+
+function (p::IceFloeSegmentationAlgorithm)(
+    truecolor::T₁,
+    falsecolor::T₂,
+    landmask::T₃;
+    intermediate_results_callback::Union{Nothing,Function}=nothing,
+) where {
+    T₁<:AbstractMatrix{<:Union{AbstractRGB,TransparentRGB}},
+    T₂<:AbstractMatrix{<:Union{AbstractRGB,TransparentRGB}},
+    T₃<:AbstractMatrix{<:Union{Bool,Gray{Bool},AbstractRGB,TransparentRGB}},
+}
+    landmask, coastal_buffer_mask = create_landmask(
+        float64.(landmask), p.coastal_buffer_structuring_element
+    )
+    return p(
+        truecolor,
+        falsecolor,
+        landmask,
+        coastal_buffer_mask;
+        intermediate_results_callback=intermediate_results_callback,
+    )
+end
