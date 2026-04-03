56b5431746389206163b332ec8c0508ced17f770
John Gerrard Holland <john_holland1@brown.edu>
2026-04-03 17:07:31 +0000

    fix type of test_similarity

--- test/test_error_rate.jl
+++ test/test_error_rate.jl
@@ -1,5 +1,7 @@
 # TODO: make this into a file not named test...
-function test_similarity(imgA::BitMatrix, imgB::BitMatrix, error_rate=0.005)
+function test_similarity(
+    imgA::AbstractArray{Bool}, imgB::AbstractArray{Bool}, error_rate=0.005
+)
     error = sum(imgA .!== imgB) / prod(size(imgA))
     res = error_rate > error
     if res
