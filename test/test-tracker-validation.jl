062c36bcbcb6d6b536cc4b0a7bbd8a9aefd1eb3b
John Gerrard Holland <john_holland1@brown.edu>
2026-04-06 13:47:58 +0000

    simplify the known broken cases tests

--- test/test-tracker-validation.jl
+++ test/test-tracker-validation.jl
@@ -49,154 +49,26 @@
     end
 end
 
-@testitem "FloeTracker – validated pairs" setup = [TrackerValidation] tags = [:e2e] begin
-    import DataFrames: DataFrame
-    import IceFloeTracker:
-        FloeTracker, FilterFunction, MinimumWeightMatchingFunction, Watkins2026Dataset
-
-    # Each pair is a set of two satellite passes (aqua and terra) for the same
-    # location and date in the Watkins 2026 validation dataset.
-    # The pairs listed here are those case_numbers for which both passes have
-    # validated labeled floe data (fl_analyst != "").
-    dataset = Watkins2026Dataset(; ref="v0.1")
-    tracker = FloeTracker(;
-        filter_function=FilterFunction(), matching_function=MinimumWeightMatchingFunction()
-    )
-
-    @test tracker_runs_without_error(dataset, 1, tracker)
-    @test tracker_runs_without_error(dataset, 4, tracker)
-    @test tracker_runs_without_error(dataset, 5, tracker)
-    @test tracker_runs_without_error(dataset, 6, tracker)
-    @test tracker_runs_without_error(dataset, 7, tracker)
-    @test tracker_runs_without_error(dataset, 8, tracker)
-    @test tracker_runs_without_error(dataset, 9, tracker)
-    @test tracker_runs_without_error(dataset, 10, tracker)
-    @test tracker_runs_without_error(dataset, 11, tracker)
-    @test tracker_runs_without_error(dataset, 12, tracker)
-    @test tracker_runs_without_error(dataset, 13, tracker)
-    @test tracker_runs_without_error(dataset, 14, tracker)
-    @test tracker_runs_without_error(dataset, 15, tracker)
-    @test tracker_runs_without_error(dataset, 16, tracker)
-    @test tracker_runs_without_error(dataset, 17, tracker)
-    @test tracker_runs_without_error(dataset, 18, tracker)
-    @test tracker_runs_without_error(dataset, 19, tracker)
-    @test tracker_runs_without_error(dataset, 21, tracker)
-    @test tracker_runs_without_error(dataset, 22, tracker)
-    @test tracker_runs_without_error(dataset, 23, tracker)
-    @test tracker_runs_without_error(dataset, 25, tracker)
-    @test tracker_runs_without_error(dataset, 29, tracker)
-    @test tracker_runs_without_error(dataset, 32, tracker)
-    @test tracker_runs_without_error(dataset, 33, tracker)
-    @test tracker_runs_without_error(dataset, 34, tracker)
-    @test tracker_runs_without_error(dataset, 36, tracker)
-    @test tracker_runs_without_error(dataset, 37, tracker)
-    @test tracker_runs_without_error(dataset, 39, tracker)
-    @test tracker_runs_without_error(dataset, 43, tracker)
-    @test tracker_runs_without_error(dataset, 44, tracker)
-    @test tracker_runs_without_error(dataset, 46, tracker)
-    @test tracker_runs_without_error(dataset, 47, tracker)
-    @test tracker_runs_without_error(dataset, 48, tracker)
-    @test tracker_runs_without_error(dataset, 49, tracker)
-    @test tracker_runs_without_error(dataset, 50, tracker)
-    @test tracker_runs_without_error(dataset, 51, tracker)
-    @test tracker_runs_without_error(dataset, 52, tracker)
-    @test tracker_runs_without_error(dataset, 53, tracker) broken = true
-    @test tracker_runs_without_error(dataset, 54, tracker)
-    @test tracker_runs_without_error(dataset, 55, tracker)
-    @test tracker_runs_without_error(dataset, 56, tracker)
-    @test tracker_runs_without_error(dataset, 57, tracker)
-    @test tracker_runs_without_error(dataset, 58, tracker)
-    @test tracker_runs_without_error(dataset, 61, tracker)
-    @test tracker_runs_without_error(dataset, 62, tracker)
-    @test tracker_runs_without_error(dataset, 63, tracker)
-    @test tracker_runs_without_error(dataset, 65, tracker)
-    @test tracker_runs_without_error(dataset, 67, tracker)
-    @test tracker_runs_without_error(dataset, 68, tracker)
-    @test tracker_runs_without_error(dataset, 69, tracker)
-    @test tracker_runs_without_error(dataset, 70, tracker)
-    @test tracker_runs_without_error(dataset, 71, tracker)
-    @test tracker_runs_without_error(dataset, 73, tracker)
-    @test tracker_runs_without_error(dataset, 75, tracker)
-    @test tracker_runs_without_error(dataset, 77, tracker)
-    @test tracker_runs_without_error(dataset, 80, tracker)
-    @test tracker_runs_without_error(dataset, 81, tracker)
-    @test tracker_runs_without_error(dataset, 83, tracker)
-    @test tracker_runs_without_error(dataset, 84, tracker) broken = true
-    @test tracker_runs_without_error(dataset, 86, tracker)
-    @test tracker_runs_without_error(dataset, 87, tracker)
-    @test tracker_runs_without_error(dataset, 93, tracker)
-    @test tracker_runs_without_error(dataset, 95, tracker)
-    @test tracker_runs_without_error(dataset, 97, tracker)
-    @test tracker_runs_without_error(dataset, 98, tracker)
-    @test tracker_runs_without_error(dataset, 99, tracker)
-    @test tracker_runs_without_error(dataset, 100, tracker)
-    @test tracker_runs_without_error(dataset, 103, tracker)
-    @test tracker_runs_without_error(dataset, 104, tracker)
-    @test tracker_runs_without_error(dataset, 105, tracker) broken = true
-    @test tracker_runs_without_error(dataset, 106, tracker)
-    @test tracker_runs_without_error(dataset, 107, tracker)
-    @test tracker_runs_without_error(dataset, 108, tracker)
-    @test tracker_runs_without_error(dataset, 109, tracker)
-    @test tracker_runs_without_error(dataset, 110, tracker)
-    @test tracker_runs_without_error(dataset, 111, tracker)
-    @test tracker_runs_without_error(dataset, 112, tracker)
-    @test tracker_runs_without_error(dataset, 115, tracker)
-    @test tracker_runs_without_error(dataset, 116, tracker)
-    @test tracker_runs_without_error(dataset, 117, tracker)
-    @test tracker_runs_without_error(dataset, 118, tracker)
-    @test tracker_runs_without_error(dataset, 119, tracker)
-    @test tracker_runs_without_error(dataset, 121, tracker)
-    @test tracker_runs_without_error(dataset, 122, tracker)
-    @test tracker_runs_without_error(dataset, 128, tracker)
-    @test tracker_runs_without_error(dataset, 129, tracker)
-    @test tracker_runs_without_error(dataset, 130, tracker)
-    @test tracker_runs_without_error(dataset, 132, tracker)
-    @test tracker_runs_without_error(dataset, 133, tracker)
-    @test tracker_runs_without_error(dataset, 135, tracker)
-    @test tracker_runs_without_error(dataset, 136, tracker)
-    @test tracker_runs_without_error(dataset, 138, tracker)
-    @test tracker_runs_without_error(dataset, 140, tracker)
-    @test tracker_runs_without_error(dataset, 141, tracker) broken = true
-    @test tracker_runs_without_error(dataset, 142, tracker) broken = true
-    @test tracker_runs_without_error(dataset, 144, tracker)
-    @test tracker_runs_without_error(dataset, 146, tracker)
-    @test tracker_runs_without_error(dataset, 148, tracker)
-    @test tracker_runs_without_error(dataset, 150, tracker)
-    @test tracker_runs_without_error(dataset, 152, tracker)
-    @test tracker_runs_without_error(dataset, 155, tracker)
-    @test tracker_runs_without_error(dataset, 156, tracker)
-    @test tracker_runs_without_error(dataset, 157, tracker)
-    @test tracker_runs_without_error(dataset, 158, tracker)
-    @test tracker_runs_without_error(dataset, 160, tracker)
-    @test tracker_runs_without_error(dataset, 161, tracker)
-    @test tracker_runs_without_error(dataset, 164, tracker)
-    @test tracker_runs_without_error(dataset, 166, tracker)
-    @test tracker_runs_without_error(dataset, 168, tracker)
-    @test tracker_runs_without_error(dataset, 171, tracker)
-    @test tracker_runs_without_error(dataset, 175, tracker)
-    @test tracker_runs_without_error(dataset, 186, tracker)
-    @test tracker_runs_without_error(dataset, 188, tracker) broken = true
-    @test tracker_runs_without_error(dataset, 189, tracker)
-end
-
 @testitem "FloeTracker – sample of cases" setup = [TrackerValidation] tags = [:e2e] begin
-    import IceFloeTracker:
-        FloeTracker, FilterFunction, MinimumWeightMatchingFunction, Watkins2026Dataset
-
     # Iterates through a sample of common pairs from the Watkins 2026 validation dataset,
     # loading the validated labeled floes and pass times, and running the default tracker.
-    # Selects every 7th case number from among the set of common pairs (case_numbers for
-    # which both satellite passes have validated labeled floe data).
     dataset = Watkins2026Dataset(; ref="v0.1")
     tracker = FloeTracker(;
         filter_function=FilterFunction(), matching_function=MinimumWeightMatchingFunction()
     )
 
-    for case_number in
-        [7, 14, 21, 49, 56, 63, 70, 77, 98, 112, 119, 133, 140, 161, 168, 175, 189]
-        @test tracker_runs_without_error(dataset, case_number, tracker)
-    end
-    for case_number in [53, 84, 105, 141, 142, 188]
+    all_cases_with_validated_floes = case_number.(filter(c -> c.fl_analyst != "", dataset))
+
+    # Some cases are known to be broken due to issues in the tracker.
+    # Each of these cases should be fixed before being removed from this list.
+    known_broken_cases = [53, 84, 105, 141, 142, 188]
+    for case_number in known_broken_cases
         @test tracker_runs_without_error(dataset, case_number, tracker) broken = true
+    end
+
+    # The rest of the cases should run without error.
+    working_cases = setdiff(all_cases_with_validated_floes, known_broken_cases)
+    for case_number in working_cases
+        @test tracker_runs_without_error(dataset, case_number, tracker)
     end
 end
