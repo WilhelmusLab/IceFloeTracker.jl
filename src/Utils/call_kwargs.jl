641a3f1722224e73ea4a3a8d5d81f1a763353321
John Gerrard Holland <john_holland1@brown.edu>
2026-04-08 18:40:07 +0000

    add call_kwargs

---
+++ src/Utils/call_kwargs.jl
@@ -0,0 +1,13 @@
+"""
+    call_kwargs(; name_to_function_map...)
+
+Factory function for a callback which passes keyword arguments to functions specified in `name_to_function_map`. 
+The callback can be used to pass intermediate results from a segmentation algorithm to user-specified functions, such as saving to disk or logging.
+"""
+function call_kwargs(; name_to_function_map...)
+    return function callback(; kwargs...)
+        for (name, func) in name_to_function_map
+            func(kwargs[name])
+        end
+    end
+end
