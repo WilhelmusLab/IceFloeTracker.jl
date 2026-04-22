6dc0d577069608bbc467680cc19a949c01e3b826
John Gerrard Holland <john_holland1@brown.edu>
2026-04-22 20:17:00 +0000

    simplify loader to just use the "load" function to validate whether a file can be loaded

--- src/Data/loader.jl
+++ src/Data/loader.jl
@@ -17,45 +17,41 @@ function (p::GitHubLoader)(file::AbstractString)::AbstractString
     return data
 end
 
-function _can_open_file(file_path::AbstractString)::Bool
-    try
-        !isfile(file_path) && return false
-        filesize(file_path) == 0 && return false
-        open(file_path, "r") do io
-            read(io, UInt8) # read a single UInt8 from the file
-        end
-        return true
-    catch e
-        @debug "file validation failed at $(file_path)" exception = e
-        return false
-    end
-end
-
 function _get_file(
     file_url::AbstractString,
     file_path::AbstractString;
     max_attempts::Integer=3,
     download_fn::Function=download,
-    validate_fn::Function=_can_open_file,
+    validate_fn::Function=load,
 )::AbstractString
-    max_attempts < 1 && throw(ArgumentError("max_attempts must be at least 1."))
     mkpath(dirname(file_path))
+
+    if isfile(file_path)
+        try
+            validate_fn(file_path)
+            return file_path
+        catch e
+            @info "file validation failed, removing $(file_path)" exception = e
+            rm(file_path; force=true)
+        end
+    end
+
     for attempt in 1:max_attempts
-        isfile(file_path) && validate_fn(file_path) && return file_path
-        isfile(file_path) && rm(file_path; force=true)
+        @show attempt
         try
             download_fn(file_url, file_path)
-            validate_fn(file_path) && return file_path
+            validate_fn(file_path)
+            return file_path
         catch e
             if isa(e, RequestError) && e.code != 429
-                @debug "download attempt $(attempt) failed for $(file_url)" exception = e
-                attempt < max_attempts && continue
+                @debug "download failed for $(file_url) on attempt $(attempt)" exception = e
+                continue
             else
                 rethrow(e)
             end
         end
     end
-    return error(
-        "failed to fetch valid file from $(file_url) after $(max_attempts) attempts"
+    throw(
+        ErrorException("Failed to get file from $(file_url) after $(max_attempts) attempts")
     )
 end
