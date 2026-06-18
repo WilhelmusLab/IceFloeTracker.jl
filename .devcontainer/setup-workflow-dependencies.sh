5660c9a53c9582dcdece2147e98abb07f2d3e165
John Gerrard Holland <john_holland1@brown.edu>
2026-06-18 17:05:27 +0000

    remove duplicate -y argument in apt install

--- .devcontainer/setup-workflow-dependencies.sh
+++ .devcontainer/setup-workflow-dependencies.sh
@@ -2,7 +2,7 @@
 
 set -xeuo pipefail
 sudo apt update
-sudo apt install -y libgl1 libgdal-dev libglib2.0-0 -y
+sudo apt install -y libgl1 libgdal-dev libglib2.0-0
 
 # Set up Snakemake and tools needed in the snakemake workflow
 pipx install snakemake
