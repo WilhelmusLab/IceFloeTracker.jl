ed857a5e526c3a5503b47040e5107449c05728a4
Carlos Paniagua <cpaniaguam@gmail.com>
2026-09-11 11:30:38 -0400

    Update add_ψs! to derive ψ-s curve from :boundary after add_boundary!

--- src/Tracking/floe_tracker.jl
+++ src/Tracking/floe_tracker.jl
@@ -119,10 +119,11 @@ function (t::FloeTracker)(
     add_uuids!.(props)
     !issorted(image_times) && @warn "Passtimes are not in ascending order."
     add_passtimes!.(props, image_times) # TODO: Change function name to image_times
-    add_ψs!.(props)
     # boundary curves for the boundary-based filters/registration; needs :mask,
-    # which is already requested above
+    # which is already requested above. Must precede add_ψs!, which now derives the
+    # ψ-s curve from :boundary rather than tracing the mask a second time.
     add_boundary!.(props)
+    add_ψs!.(props)
 
     tracking_results = floe_tracker(
         props,
